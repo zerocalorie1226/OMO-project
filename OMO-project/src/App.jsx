3dead0cc77f935406fe605530112cef6e24a2dd3
hwanghaein <hhi9037@naver.com>
2023-07-25 15:19:07 +0900

    [chore] merge 후 수정

--- OMO-project/src/App.jsx
+++ OMO-project/src/App.jsx
@@ -1,16 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.module.css';
-// import Main from './pages/main/Main';
-import Eating from './pages/sub/eating/Eating';
+import Main from './pages/main/Main';
+// import Eating from './pages/sub/eating/Eating';
 
 function App() {
   return (
     <>
       <div>
-      {/* <Main></Main> */}
-      <Eating></Eating>
+      <Main></Main>
+      {/* <Eating></Eating> */}
       </div>
     </>
   );
