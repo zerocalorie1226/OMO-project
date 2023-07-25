6a4f656b733b660c9e31f7c8a697c4c39a109c9e
hwanghaein <hhi9037@naver.com>
2023-07-25 14:06:05 +0900

    [style] 메인페이지 카테고리 컴포넌트 완성 및 배치

---
+++ OMO-project/src/const/main.js
@@ -0,0 +1,38 @@
+import eating from "../assets/main/먹거리 아이콘.png";
+import watching from "../assets/main/볼거리 아이콘.png";
+import playing from "../assets/main/놀거리 아이콘.png";
+import cafe from "../assets/main/카페 아이콘.png";
+import themeCafe from "../assets/main/테마카페 아이콘.png";
+
+
+export const main = [
+  {
+    id: 1,
+    title: "먹거리",
+    src: eating,
+  },
+  {
+    id: 2,
+    title: "볼거리",
+    src: watching,
+  },
+  {
+    id: 3,
+    title: "놀거리",
+    src: playing,
+  },
+  {
+    id: 4,
+    title: "카페",
+    src: cafe,
+  },
+  {
+    id: 5,
+    title: "테마\n카페",
+    src: themeCafe,
+  },
+  {
+    id: 6,
+    title: "Coming\nsoon..",
+  },
+];
--- OMO-project/src/pages/main/Main.jsx
+++ OMO-project/src/pages/main/Main.jsx
@@ -1,12 +1,17 @@
-import styles from './Main.module.css';
+import styles from "./Main.module.css";
+import {CategoryBox} from "../../components/CategoryBox/CategoryBox";
+import {main} from "./../../const/main";
 
-
-function Main(){
-  return(
+function Main() {
+  return (
     <div>
-      <div className={styles['main']}>라라라</div>
-   </div>
-  )
+      <div className={styles["main-category-container"]}>
+        {main.map((el) => {
+          return <CategoryBox key={el.id} title={el.title} img={el.src} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Main;
