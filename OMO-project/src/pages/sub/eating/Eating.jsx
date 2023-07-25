9d9d2e23aefaf75773cd0ff3943926145bf57af7
hwanghaein <hhi9037@naver.com>
2023-07-25 15:06:27 +0900

    [chore] 불필요한 css 파일 삭제 및 경로 수정

--- OMO-project/src/pages/sub/eating/Eating.jsx
+++ OMO-project/src/pages/sub/eating/Eating.jsx
@@ -1,4 +1,4 @@
-import styles from "./Eating.module.css";
+import styles from "./../../main/Main.module.css";
 import { SubCategoryBox } from './../../../components/SubCategoryBox/SubCategoryBox';
 import {sub} from "./../../../const/sub";
 
@@ -6,7 +6,7 @@ import {sub} from "./../../../const/sub";
 function Eating() {
   return (
     <div>
-      <div className={styles["sub-category-container"]}>
+      <div className={styles["main-category-container"]}>
         {sub.map((el) => {
           return <SubCategoryBox key={el.id} title={el.title} img={el.src} />;
         })}
