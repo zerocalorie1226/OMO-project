ec777ab533b00e049068a342305e78769376b2cf
Sungchan CHo <kjyy07@naver.com>
2023-07-30 12:50:57 +0900

    [style] hover 시 그림자 추가

--- OMO-project/src/components/CategoryBox/CategoryBox.jsx
+++ OMO-project/src/components/CategoryBox/CategoryBox.jsx
@@ -2,11 +2,11 @@ import styles from "./CategoryBox.module.css";
 
 export const CategoryBox = (props) => (
   <div>
-    <div className={styles["main-category-box"]}>
-      <span className={styles["main-category-box-title"]}>{props.title}</span>
-      <img className={styles["main-category-box-img"]} src={props.img} />
-    </div>
+    <a href="#" className={styles["main-category-container"]}>
+      <div className={styles["main-category-box"]}>
+        <span className={styles["main-category-box-title"]}>{props.title}</span>
+        <img className={styles["main-category-box-img"]} src={props.img} />
+      </div>
+    </a>
   </div>
 );
-
-
