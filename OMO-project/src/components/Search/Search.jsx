54743f4a94e6eedf5c40f71042acf88894f26526
hwanghaein <hhi9037@naver.com>
2023-07-30 18:29:55 +0900

    [style] 검색창 class name 수정

--- OMO-project/src/components/Search/Search.jsx
+++ OMO-project/src/components/Search/Search.jsx
@@ -4,14 +4,14 @@ import Magnifier from "../../assets/magnifier.png";
 
 export function Search() {
   return (
-    <div className={styles["main-container"]}>
-      <div className={styles["main-img-container"]}>
+    <div className={styles["main-search-container"]}>
+      <div className={styles["main-search-img-container"]}>
         <img src={LogoCircle} alt="로고 동그라미" />
         <a href="#">
           <img src={Magnifier} alt="돋보기" />
         </a>
       </div>
-      <input className={styles["main-search-container"]} placeholder="장소를 입력해주세요" />
+      <input className={styles["main-search-input-container"]} placeholder="장소를 입력해주세요" />
     </div>
   );
 }
