dc36e405dcf17a669f5e37cd0c323c92a925313d
hwanghaein <hhi9037@naver.com>
2023-07-30 18:28:50 +0900

    [style] 메인페이지에 날씨 배너 추가

---
+++ OMO-project/src/components/CategoryBox/Weather/Weather.jsx
@@ -0,0 +1,12 @@
+import styles from "./Weather.module.css";
+import WeatherBanner from "../../../assets/main/weather-banner.png";
+
+export function Weather() {
+  return (
+    <>
+       <div className={styles["main-weather-container"]}>
+       <img className={styles["weather-banner"]} src={WeatherBanner} alt="날씨 배너" />
+       </div>
+    </>
+  );
+}
--- OMO-project/src/pages/main/Main.jsx
+++ OMO-project/src/pages/main/Main.jsx
@@ -1,11 +1,16 @@
 import styles from "./Main.module.css";
 import {CategoryBox} from "../../components/CategoryBox/CategoryBox";
 import {main} from "./../../const/main";
+import { Search } from '../../components/Search/Search';
+import { Weather } from "../../components/CategoryBox/Weather/Weather";
+
 
 
 function Main() {
   return (
     <div>
+      <Search />
+      <Weather />
       <div className={styles["main-category-container"]}>
         {main.map((el) => {
           return <CategoryBox key={el.id} title={el.title} img={el.src} />;
