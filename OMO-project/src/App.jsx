1823c3e67d451f4c5388e042a596ca2727ea5c25
hwanghaein <hhi9037@naver.com>
2023-07-30 12:11:20 +0900

    [chore] 컴포넌트 표기 수정 및 화살표 함수으로 부분 수정

--- OMO-project/src/App.jsx
+++ OMO-project/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.module.css';
 import { Header } from './components/Header/Header';
 import { Search } from './components/Search/Search';
-// import Main from './pages/main/Main';
+import Main from './pages/main/Main';
 import Signup from './pages/signup/Signup';
-// import Eating from './pages/sub/eating/Eating';
-// import Watching from './pages/sub/watching/Watching';
-// import Playing from './pages/sub/playing/Playing';
-// import ThemeCafe from './pages/sub/theme-cafe/ThemeCafe';
+import Eating from './pages/sub/eating/Eating';
+import Watching from './pages/sub/watching/Watching';
+import Playing from './pages/sub/playing/Playing';
+import ThemeCafe from './pages/sub/theme-cafe/ThemeCafe';
 
 
 function App() {
@@ -14,13 +14,13 @@ function App() {
     <>
       <div>
       <Header />
-      {/* <Search /> */}
-      <Signup />
-      {/* <Main></Main> */}
-      {/* <Eating></Eating> */}
-      {/* <Watching></Watching> */}
-      {/* <Playing></Playing> */}
-      {/* <ThemeCafe></ThemeCafe> */}
+      <Search />
+      {/* <Signup /> */}
+      <Main />
+      {/* <Eating /> */}
+      {/* <Watching /> */}
+      {/* <Playing /> */}
+      {/* <ThemeCafe /> */}
       </div>
     </>
   );
--- OMO-project/src/components/CategoryBox/CategoryBox.jsx
+++ OMO-project/src/components/CategoryBox/CategoryBox.jsx
@@ -1,13 +1,12 @@
 import styles from "./CategoryBox.module.css";
 
-export function CategoryBox(props) {
-  return (
-    <div>
-      <div className={styles["main-category-box"]}>
-        <span className={styles["main-category-box-title"]}>{props.title}</span>
-        <img className={styles["main-category-box-img"]} src={props.img} />
-      </div>
+export const CategoryBox = (props) => (
+  <div>
+    <div className={styles["main-category-box"]}>
+      <span className={styles["main-category-box-title"]}>{props.title}</span>
+      <img className={styles["main-category-box-img"]} src={props.img} />
     </div>
-  );
-}
+  </div>
+);
+
 
