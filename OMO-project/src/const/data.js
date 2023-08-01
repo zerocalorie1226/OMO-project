a4637c9a18c9a2de46207c91a209e74ffc0b1e02
hwanghaein <hhi9037@naver.com>
2023-08-01 16:35:54 +0900

    [style] 리스트 페이지 데이터 구성

---
+++ OMO-project/src/const/data.js
@@ -0,0 +1,68 @@
+import Ramen1 from "../assets/list/ramen-img1.png";
+import Ramen2 from "../assets/list/ramen-img2.png";
+import Map from "../assets/detail/google-map.png"
+
+
+export const data = [
+  {
+    id: 1,
+    title: "이치라멘&돈까스",
+    like: "999+",
+    jjim: "999+",
+    addressBrief: "인천광역시 미추홀구 용현동",
+    addressStreet: "인천광역시 미추홀구 경인남길 30번길 95",
+    addressNumber: "인천광역시 미추홀구 용현동 186-17",
+    intro: "호준이가 진짜 맛있다고 주장하는 집",
+    time: "17:00~2:00",
+    runTime: "영업 중",
+    holiday: "일요일",
+    menu: "전기구이 통닭 (1마리(국내산))",
+    price: "16,000원",
+    call: "032-872-7787",
+    googleMap: Map,
+    src1: Ramen1,
+    src2: Ramen2,
+  },
+
+  {
+    id: 2,
+    title: "유가네 닭갈비",
+    like: "999+",
+    jjim: "999+",
+    addressBrief: "울산광역시 북구 신천동",
+    addressStreet: "울산광역시 북구 매곡1로 55-21",
+    addressNumber: "울산광역시 북구 신천동 109-5",
+    intro: "혜인이가 젤 좋아하는 닭갈비집",
+    time: "11:00~22:00",
+    runTime: "영업 중",
+    holiday: "없음",
+    menu: "유가네 닭갈비 (1마리(국내산))",
+    price: "19,000원",
+    call: "032-281-7197",
+    googleMap: Map,
+    src1: Ramen1,
+    src2: Ramen2,
+  },
+
+  {
+    id: 3,
+    title: "와우리순대국",
+    like: "999+",
+    jjim: "999+",
+    addressBrief: "경기도 안양시 안양동",
+    addressStreet: "경기도 안양시 만안구 성결대학로 34-1",
+    addressNumber: "경기도 안양시 안양동 563-24",
+    intro: "성찬이가 진짜 맛있다고 주장하는 집",
+    time: "9:00~21:00",
+    runTime: "영업 중",
+    holiday: "없음",
+    menu: "내장 순대국",
+    price: "9000원",
+    call: "031-448-8292",
+    googleMap: Map,
+    src1: Ramen1,
+    src2: Ramen2,
+  },
+
+  
+];
