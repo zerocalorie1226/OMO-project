e17f4c7fe8e3377f8ee909c3f85fe394038601b7
hwanghaein <hhi9037@naver.com>
2023-08-01 16:37:14 +0900

    [style] 리스트 페이지 List Box 컴포넌트 생성 및 불러오기

--- OMO-project/src/pages/list/List.jsx
+++ OMO-project/src/pages/list/List.jsx
@@ -1,6 +1,9 @@
 import styles from "./List.module.css";
 import Filter from "../../components/Fliter/Filter";
 import ListSearch from "../../components/ListSearch/ListSearch";
+import {data} from "./../../const/data";
+import { ListBox } from './../../components/ListBox/ListBox';
+
 
 const List = () => (
   <>
@@ -8,8 +11,15 @@ const List = () => (
       <Filter />
       <ListSearch />
     </div>
-    <div className={styles["list-list-container"]}></div>
+    <section className={styles["list-list-container"]}>
+      <div  className={styles["list-list-box-container"]}>
+    {data.map((el) => {
+        return <ListBox key={el.id} title={el.title} like={el.like} jjim={el.jjim} runTime={el.runTime} intro={el.intro} addressBrief={el.addressBrief}  img1={el.src1} img2={el.src2}/>;
+      })}
+      </div>
+    </section>
   </>
 );
 
 export default List;
+
