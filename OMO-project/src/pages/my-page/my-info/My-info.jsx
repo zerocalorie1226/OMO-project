460a2b40c049458f4ec56a6aa5847586a1a0239a
Sungchan CHo <kjyy07@naver.com>
2023-08-03 17:16:11 +0900

    [markup] hr태그 배치

--- OMO-project/src/pages/my-page/my-info/My-info.jsx
+++ OMO-project/src/pages/my-page/my-info/My-info.jsx
@@ -3,18 +3,16 @@ import styles from "./My-info.module.css";
 import Mypage from "../../../components/Mypage/Mypage";
 import LogoCircle from "../../../assets/logo-circle.png";
 import MyInfoIcon from "../../../assets/my-page/my-info/my-info.png";
-// import MyInfoHeart from "../../../assets/my-page/my-info/empty-heart.png";
-// import MyInfoWrite from "../../../assets/my-page/my-info/my-writing.png";
-// import MyInfoSetting from "../../../assets/my-page/my-info/profile-setting.png";
-// import MyInfoRecent from "../../../assets/my-page/my-info/recent-place.png";
-// import MyInfoThumb from "../../../assets/my-page/my-info/empty-thumb.png";
 
 const MyInfo = () => (
   <>
     <h2 className={styles["myInfo-title-container"]}>
       <img src={MyInfoIcon} alt="내 정보 아이콘" /> 내 정보
     </h2>
-    <Mypage />
+    <div className={styles["myInfo-categories-hr-container"]}>
+      <Mypage />
+      <hr className={styles["myInfo-hr-container"]} />
+    </div>
   </>
 );
 
