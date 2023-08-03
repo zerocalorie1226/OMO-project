d9a37a4155b2b1a03b2404326ebdc2bbd091679f
hwanghaein <hhi9037@naver.com>
2023-08-03 14:46:26 +0900

    [style] Top 버튼 컴포넌트 생성 및 구현

--- OMO-project/src/components/ScrollToTop/ScrollToTop.jsx
+++ OMO-project/src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,13 +1,54 @@
 import styles from "./ScrollToTop.module.css";
-import { TopButton } from './../../components/ScrollToTop/TopButton/TopButton';
+import TopButtonImg from "../../assets/top-button.png";
+import { useState, useEffect } from 'react'
 
 
 
-export const ScrollToTop = ({ handleClick }) => (
-    <>
-    <div className={styles["position-container"]}>
-      <TopButton onClick={handleClick}></TopButton>
-      </div>
-    </>
-  );
+export function ScrollToTop() {
+
+  const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
+  
+  const handleFollow = () => {
+    // setScrollY(window.pageYOffset);
+    if(window.ScrollY > 100) {
+      // 100 이상이면 버튼이 보이게
+      setBtnStatus(true);
+    } else {
+      // 100 이하면 버튼이 사라지게
+      setBtnStatus(false);
+    }
+  }
+
+  const handleTop = () => {  // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+
+  }
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow)
+    }
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleFollow)
+    }
+  })
+
+  return(
+  <>
+    <div className={styles["topbtn-container"]}>
+      <button 
+        className={styles["topbtn"]} 
+        onClick={handleTop}  // 버튼 클릭시 함수 호출
+      ><img src={TopButtonImg} alt="탑버튼 아이콘"/></button>
+
+    </div>
+  </>
+);
+    }
+
+
 
--- OMO-project/src/pages/list/List.jsx
+++ OMO-project/src/pages/list/List.jsx
@@ -3,13 +3,46 @@ import Filter from "../../components/Fliter/Filter";
 import ListSearch from "../../components/ListSearch/ListSearch";
 import {data} from "./../../const/data";
 import { ListBox } from './../../components/ListBox/ListBox';
-import { ScrollToTop } from './../../components/ScrollToTop/ScrollToTop';
-import handleScroll from './../../components/ScrollToTop/HandleScroll.js';
-import TopButtonImg from "../../assets/top-button.png"
+import { useState, useEffect } from 'react'
+import { ScrollToTop } from "../../components/ScrollToTop/ScrollToTop";
 
 
+function List() {
 
-const List = () => (
+  const [ScrollY, setScrollY] = useState(0);
+  const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
+  
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+    if(ScrollY > 100) {
+      // 100 이상이면 버튼이 보이게
+      setBtnStatus(true);
+    } else {
+      // 100 이하면 버튼이 사라지게
+      setBtnStatus(false);
+    }
+  }
+
+  const handleTop = () => {  // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    setScrollY(0);  // ScrollY 의 값을 초기화
+    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
+  }
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow)
+    }
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleFollow)
+    }
+  })
+
+  return(
   <>
     <div className={styles["list-component-container"]}>
       <Filter />
@@ -22,11 +55,11 @@ const List = () => (
       })}
       </div>
     </section>
-    {/* <a href="#"><img src={TopButtonImg} alt="탑버튼 아이콘"/></a> */}
-    <ScrollToTop handleClick={handleScroll} />
-  
+    <ScrollToTop/>
+
   </>
 );
+    }
 
 export default List;
 
