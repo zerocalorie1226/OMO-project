da059fe87410c51a7255018b5e0de83be286e3e6
Sungchan CHo <kjyy07@naver.com>
2023-08-09 16:07:15 +0900

    [markup] Mypage 비밀번호 변경 컴포넌트 마크업 구현

--- OMO-project/src/pages/Mypage/ProfileSetting/ProfileSetting.jsx
+++ OMO-project/src/pages/Mypage/ProfileSetting/ProfileSetting.jsx
@@ -2,6 +2,7 @@ import styles from "./ProfileSetting.module.css";
 import SettingIcon from "../../../assets/my-page/setting/profile-setting.png";
 import Mypage from "../../../components/Mypage/Mypage";
 import ProfileChange from "../../../components/MypageProfileSetting/ProfileChange/ProfileChange";
+import PasswordChange from "../../../components/MypageProfileSetting/PasswordChange/PasswordChange";
 
 const ProfileSetting = () => (
   <div className={styles["profileSetting-total-container"]}>
@@ -11,15 +12,8 @@ const ProfileSetting = () => (
     <div className={styles["profileSetting-categories-main-container"]}>
       <Mypage />
       <div className={styles["profileSetting-main-container"]}>
-        {/* <div className={styles["profileSetting-main-profileChange-container"]}>
-          <p className={styles["profileSetting-main-profileChange-title"]}>프로필 변경</p>
-          <img src={ProfileImg} alt="프로필 사진" className={styles["profileSetting-main-profileChange-img"]} />
-          <a className={styles["profileSetting-main-profileChange-background"]}></a>
-          <img src={ImgChange} alt="이미지 변경" className={styles["profileSetting-main-profileChange-changeImg"]} />
-          <a href="#" className={styles["profileSetting-main-profileChange-click"]}></a>
-        </div> */}
         <ProfileChange />
-        <div className={styles["profileSetting-main-passwordChange-container"]}></div>
+        <PasswordChange />
       </div>
     </div>
   </div>
