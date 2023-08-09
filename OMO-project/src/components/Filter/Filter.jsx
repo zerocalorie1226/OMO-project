2e721ca65175e5fbe35264853bc96ad1bed99c08
Sungchan CHo <kjyy07@naver.com>
2023-08-09 13:34:27 +0900

    [markup] List페이지 filter 마크업 수정

--- OMO-project/src/components/Filter/Filter.jsx
+++ OMO-project/src/components/Filter/Filter.jsx
@@ -2,21 +2,21 @@ import styles from "./Filter.module.css";
 
 const Filter = () => (
   <div className={styles["filter-container"]}>
-    <a href="#" className={styles["filter-recommend"]}>
+    <button href="#" className={styles["filter-recommend"]}>
       추천순
-    </a>
+    </button>
     <p className={styles["filter-p"]}> | </p>
-    <a href="#" className={styles["filter-close"]}>
+    <button href="#" className={styles["filter-close"]}>
       가까운 순
-    </a>
+    </button>
     <p className={styles["filter-p"]}> | </p>
-    <a href="#" className={styles["filter-review"]}>
+    <button href="#" className={styles["filter-review"]}>
       리뷰 많은 순
-    </a>
+    </button>
     <p className={styles["filter-p"]}> | </p>
-    <a href="#" className={styles["filter-open"]}>
+    <button href="#" className={styles["filter-open"]}>
       영업 중
-    </a>
+    </button>
   </div>
 );
 
