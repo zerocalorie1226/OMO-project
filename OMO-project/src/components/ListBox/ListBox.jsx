b6f7a3c1f7189e78f90e5e43d6a46943fdd9c3a2
hwanghaein <hhi9037@naver.com>
2023-08-10 20:51:11 +0900

    [style] 리스트 페이지, 마이페이지 ListBox 공통 디자인 수정

--- OMO-project/src/components/ListBox/ListBox.jsx
+++ OMO-project/src/components/ListBox/ListBox.jsx
@@ -8,8 +8,16 @@ export const ListBox = (props) => (
     <a href="#" className={styles["list-box-container"]}>
     <div className={styles["list-box-inner-container"]}>
     <span className={styles["list-box-title"]}>{props.title}</span>
-    <div className={styles["list-box-like"]}> <img src={Like} alt="좋아요"/> <span className={styles["list-box-like-number"]}> {props.like}</span></div>
-    <div className={styles["list-box-jjim"]}> <img src={Jjim} alt="찜"/> <span className={styles["list-box-jjim-number"]}> {props.jjim}</span></div>
+    
+    <div className={styles["list-box-like-jjim-container"]}>
+    <div className={styles["list-box-jjim"]}>
+    <button type="button"><img src={Jjim} alt="찜 아이콘" style={{position: "absolute", top: "0px", width: "23px", height: "23px",}} /> </button> <span className={styles["list-box-jjim-number"]}> {props.jjim}</span>
+    </div>
+    <div className={styles["list-box-like"]}>
+    <button type="button"><img src={Like} alt="좋아요 아이콘" style={{position: "absolute", top: "-1px", width: "24px", height: "24px",}} /> </button><span className={styles["list-box-like-number"]}> {props.like}</span>
+    </div>
+    </div>
+
     <span className={styles["list-box-runtime"]}>{props.runTime}</span>
     <span className={styles["list-box-intro"]}>{props.intro}</span>
     <span className={styles["list-box-address-brief"]}>{props.addressBrief}</span>
