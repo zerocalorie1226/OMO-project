47d563e17158717d3a91583a1aaca7bc99f0dfc0
hwanghaein <hhi9037@naver.com>
2023-08-10 16:44:48 +0900

    [style] 상세페이지 아이콘 버튼으로 수정

--- OMO-project/src/components/DetailItems/DetailItems.jsx
+++ OMO-project/src/components/DetailItems/DetailItems.jsx
@@ -26,11 +26,11 @@ export const DetailItems = (props) => {
         <span className={styles["detail-title"]}>{data.title}</span>
         <div className={styles["detail-like-jjim-container"]}>
           <div className={styles["detail-jjim"]}>
-            <img src={Jjim} alt="찜 아이콘" style={{position: "absolute", top: "1px"}} /> <span className={styles["detail-jjim-number"]}> {data.jjim}</span>
+           <button type="button"><img src={Jjim} alt="찜 아이콘" style={{position: "absolute", top: "1px"}} /> </button> <span className={styles["detail-jjim-number"]}> {data.jjim}</span>
           </div>
           <span className={styles["detail-like-jjim-line"]}>|</span>
           <div className={styles["detail-like"]}>
-            <img src={Like} alt="좋아요 아이콘" style={{position: "absolute", top: "-1px"}} /> <span className={styles["detail-like-number"]}> {data.like}</span>
+          <button type="button"><img src={Like} alt="좋아요 아이콘" style={{position: "absolute", top: "-1px"}} /> </button><span className={styles["detail-like-number"]}> {data.like}</span>
           </div>
         </div>
       </section>
