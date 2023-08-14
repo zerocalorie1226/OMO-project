a3ee4900f3a52f0176dcd06648b7be60c68cd135
hwanghaein <hhi9037@naver.com>
2023-08-14 18:17:41 +0900

    [style] 커뮤니티 페이지 카테고리 스타일 수정

--- OMO-project/src/pages/Community/MyCourseBoard/MyCourseBoard.jsx
+++ OMO-project/src/pages/Community/MyCourseBoard/MyCourseBoard.jsx
@@ -1,10 +1,25 @@
 import styles from "./MyCourseBoard.module.css";
+import Filter from "../../../components/Filter/Filter";
 import { CommunityCategory } from './../../../components/CommunityCategory/CommunityCategory';
-
+// import {ScrollToTop} from "../../components/ScrollToTop/ScrollToTop";
+import ListSearch from './../../../components/ListSearch/ListSearch';
+import { communityPageFilter } from './../../../const/communityPageFilter';
 
 const MyCourseBoard = () => (
   <>
   <CommunityCategory />
+  <div className={styles["community-component-container"]}>
+      <div className={styles["community-filter-container"]}>
+        {communityPageFilter.map((el) => {
+          return <Filter key={el.id} title={el.title} bar={el.bar} />;
+        })}
+      </div>
+      <ListSearch />
+    </div>
+
+  <section  className={styles["community-mycourse-container"]}></section>
+  
+  {/* <ScrollToTop /> */}
   </>
 );
 
