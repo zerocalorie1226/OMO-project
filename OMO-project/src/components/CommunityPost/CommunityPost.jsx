aa8c0ed863f1deac3a0ed1d30c01510bf95410db
hwanghaein <hhi9037@naver.com>
2023-08-20 14:34:43 +0900

    [style] CommunityPost 컴포넌트 마크업 수정

--- OMO-project/src/components/CommunityPost/CommunityPost.jsx
+++ OMO-project/src/components/CommunityPost/CommunityPost.jsx
@@ -1,76 +1,82 @@
 import styles from "./CommunityPost.module.css";
-import Report from "../../../assets/community/worry-board/report.jpg"
-import Like from "../../../assets/detail/empty-thumb.png";
-import Comment from "../../../assets/detail/review.png";
+import Report from "../../assets/community/worry-board/report.png";
+import Like from "../../assets/detail/empty-thumb.png";
+import Comment from "../../assets/detail/review.png";
 
 export const CommunityPost = (props) => (
   <>
     {/* 전체 영역 */}
     <div className={styles["community-post-container"]}>
-        
-        {/* 제목 */}
-        <span className={styles["community-post-title"]}>{props.title}</span>
+      {/*버튼 전까지 윗부분 영역*/}
+      <div className={styles["community-post-content-container"]}>
+        <div className={styles["community-post-content-title-date"]}>
+          {/* 제목 */}
+          <span className={styles["community-post-title"]}>{props.title}</span>
 
-        {/* 날짜 */}
-        <span className={styles["community-post-date"]}>{props.reg_at}</span>
-        
+          {/* 날짜 */}
+          <span className={styles["community-post-date"]}>{props.reg_at}</span>
+        </div>
+
         {/* 프로필 이미지+닉네임 */}
         <div className={styles["community-post-profile"]}>
-        <img className={styles["community-post-profile-img"]} src={props.src}  alt="프로필 이미지" style={{width: "32px", height: "32px"}}  />
-        <span className={styles["community-post-profile-nick"]}>{props.nick}</span>
+          <img className={styles["community-post-profile-img"]} src={props.src} alt="프로필 이미지" style={{width: "32px", height: "32px"}} />
+          <span className={styles["community-post-profile-nick"]}>{props.nick}</span>
         </div>
 
         {/* 글 내용 */}
         <span className={styles["community-post-content"]}>{props.content}</span>
 
+       
           {/*공감수*/}
-        <span className={styles["community-post-like-number"]}>좋아요 {props.like}</span>
-        <span className={styles["community-post-view-number"]}>• 조회수 {props.view}</span>
-        <span className={styles["community-post-comment-number"]}>• 댓글 {props.comment}</span>
+          <div className={styles["community-post-number-report-wapper"]}>
+            <span className={styles["community-post-like-number"]}>좋아요 {props.like}</span>
+            <span className={styles["community-post-view-number"]}>• 조회수 {props.view}</span>
+            <span className={styles["community-post-comment-number"]}>• 댓글 {props.comment}</span>
+             {/* 신고 아이콘 */}
+          <button  className={styles["community-post-report-button"]} type="button"><img className={styles["community-post-report"]} alt="신고 아이콘" src={Report} style={{width: "32px", height: "32px"}} /></button>
+          </div>
 
-        {/* 신고 아이콘 */}
-        <img className={styles["community-post-report"]} src={Report} />
+         
+        
+      </div>
 
+      <div className={styles["community-post-button-wrapper"]}>
         {/*좋아요 버튼*/}
-        <button type="button" className={styles["community-post-like-button"]}> 
-        <img className={styles["community-post-like-button-img"]} src={Like} /> 
-        <span className={styles["community-post-like-button-text"]}>좋아요</span>
+        <button type="button" className={styles["community-post-like-button"]}>
+          <img className={styles["community-post-like-button-img"]} src={Like} />
+          좋아요
         </button>
 
         {/* 댓글달기 버튼 */}
         <button type="button" className={styles["community-post-comment-button"]}>
-        <img className={styles["community-post-comment-button-img"]} src={Comment} /> 
-        <span className={styles["community-post-comment-button-text"]}>댓글 달기</span>
+          <img className={styles["community-post-comment-button-img"]} src={Comment} />
+          댓글 달기
         </button>
+      </div>
 
-
-{/* 하단 댓글창 전체박스 */}
+      {/* 하단 댓글창 전체박스 */}
       <div className={styles["community-post-comment-container"]}>
+        {/* 댓글 입력창 */}
+        <div className={styles["community-post-comment-input-container"]}>
+            <img v className={styles["community-post-comment-input-profile-img"]} src={props.src} alt="프로필 이미지" style={{width: "50px", height: "50px"}} />
+          <input className={styles["community-post-comment-input"]} type="text" id="comment" name="comment" minlength="2" maxlength="20" size="10" placeholder="댓글을 입력하세요..."></input>
+        </div>
 
+        {/* 댓글 내용 */}
+        {props.comment_list.map((el) => (
 
-{/* 댓글 입력창 */}
-<div className={styles["community-post-comment-input-container"]}>
-       <div className={styles["community-post-comment-input-profile-img"]}> <img src={props.src} alt="프로필 이미지" style={{width: "32px", height: "32px"}} /></div>
-       <input className={styles["community-post-comment-input"]}type="text" id="comment" name="comment" minlength="2" maxlength="20" size="10" placeholder="댓글을 입력하세요..."></input>
-</div>
-
-{/* 댓글 내용 */}
-{props.comment_list.map((el) => (
-            <ul>
-              <li className={styles["community-post-comment-list"]}>
-              <div className={styles["community-post-comment-profile-img"]}> <img src={el.src} alt="프로필 이미지" style={{width: "32px", height: "32px"}} /></div>
-              <div className={styles["community-post-comment-box"]}> 
-              <span className={styles["community-post-comment-box-nick"]}>{el.nick}</span>
-              <span className={styles["community-post-comment-box-date"]}>{el.reg_at}</span>
-              <span className={styles["community-post-comment-box-content"]}>{el.content}</span>
+          <ul className={styles["community-post-comment"]}>
+            <li>
+                <img className={styles["community-post-comment-profile-img"]} src={el.src} alt="프로필 이미지" style={{width: "50px", height: "50px"}} />
+              <div className={styles["community-post-comment-box"]}>
+                <span className={styles["community-post-comment-box-nick"]}>{el.nick}</span>
+                <span className={styles["community-post-comment-box-date"]}>{el.reg_at}</span>
+                <span className={styles["community-post-comment-box-content"]}>{el.content}</span>
               </div>
-              </li>
-            </ul>
-          ))}
-
-
-</div>
-
-     </div>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </div>
   </>
 );
