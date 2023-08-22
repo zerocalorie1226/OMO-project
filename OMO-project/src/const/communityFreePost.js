388a0bffaef5ea28271d6d067e8b1df765363041
hwanghaein <hhi9037@naver.com>
2023-08-22 15:48:43 +0900

    [style] 커뮤니티 자유게시판 페이지 마크업 구현

--- OMO-project/src/const/communityFreePost.js
+++ OMO-project/src/const/communityFreePost.js
@@ -12,7 +12,7 @@ export const communityFreePost = [
     view: "70",
     comment: "4",
     comment_list: [
-      {id: 1, nick: "이니", reg_at: "2023/07/15", src: ProfileImg, content: "진짜 개노잼이네요",},
+      {id: 1, nick: "차니", reg_at: "2023/07/15", src: ProfileImg, content: "진짜 개노잼이네요",},
       {id: 2, nick: "주니", reg_at: "2023/07/14", src: ProfileImg, content: "네...?",},
       {id: 3, nick: "헌터", reg_at: "2023/07/12", src: ProfileImg, content: "무슨 드립이죠?",},
       {id: 4, nick: "키놈", reg_at: "2023/07/12", src: ProfileImg, content: "ㅋ",},
@@ -27,12 +27,11 @@ export const communityFreePost = [
     content: "특정한 목적이 없고 그저 관심이나 주목을 받기 위해, 아니면 자신이 생각하고 있는 주제긴 하더라도 그리 진지하지 않은 마인드로 올리는 글. 한 마디로 말해 글은 글이되 내용은 없는 글을 일컫는다. 디시 갤러리 등지에서 흔히 볼 수 있는 화살표글이 대표적인 뻘글이다. 뻘글이 꾸준히 같은 패턴으로 올라오면 꾸준글이 된다 내용의 성의성, 내용의 길이, 작성자의 의도를 기준으로 뻘글인지 아닌지를 파악할 수 있다. 일반적으로 별 의미가 없거나, 의미가 있는 듯 해도 내용이 한두줄 정도로 너무 짧거나, 내용이 있다고 해도 Ctrl CV 수준의 복사글 수십개가 동일 인물에 의해 난무하면 도배 뻘글로 간주된다. 동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라만세 무궁화삼천리 자전거가 좀 튼튼하고 좋아. 역시 우유를 마셔서 그런지 아주 튼튼해. 아인슈타인. 맞아요. 갑자기 그게 떠올랐어요. 양자역학. 빛의 삼원색. 막 이런게 복합적으로 이루어지면서 이번 태풍피해가 없으시길 바랄게요. 마지막으로 한 마디만 더 적자면 이렇게 딴 짓을 하게 되면 뇌에서 새로운... ",
     like: "53",
     view: "90",
-    comment: "4",
+    comment: "2",
         comment_list: [
-      {id: 1, nick: "이니", reg_at: "2023/07/04", src: ProfileImg, content: "맛집 갔다가 소품샵을 들러보는건 어떨까요?",},
-      {id: 2, nick: "주니", reg_at: "2023/07/05", src: ProfileImg, content: "저도 놀고싶어요",},
-      {id: 3, nick: "헌터", reg_at: "2023/07/06", src: ProfileImg, content: "알게되면 저도 알려주세요",},
-      {id: 4, nick: "키놈", reg_at: "2023/07/07", src: ProfileImg, content: "나만의 코스에서 찾아보세요",},
+      {id: 1, nick: "이니", reg_at: "2023/07/04", src: ProfileImg, content: "뻘글 자제 부탁요",},
+      {id: 2, nick: "주니", reg_at: "2023/07/05", src: ProfileImg, content: "무슨 글이죠?",},
+
     ],
   },
   {
