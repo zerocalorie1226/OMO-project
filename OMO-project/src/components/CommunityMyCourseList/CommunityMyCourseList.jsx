cfcff323353b97091d48d0f9f7e50b745be27242
hwanghaein <hhi9037@naver.com>
2023-08-23 00:17:27 +0900

    [chore] 커뮤니티 나만의 코스 리스트 데이터 불러오는 코드 수정

--- OMO-project/src/components/CommunityMyCourseList/CommunityMyCourseList.jsx
+++ OMO-project/src/components/CommunityMyCourseList/CommunityMyCourseList.jsx
@@ -3,12 +3,12 @@ import Like from "../../assets/community/my-course-board/empty-thumb.png"
 
 const CommunityMyCourseList = (props) => (
   <a href="#" className={styles["community-mycourse-list-container"]}>
-    <span className={styles["community-mycourse-list-title"]}>{props.title}</span>
+    <span className={styles["community-mycourse-list-title"]}>{props.data.title}</span>
     <div className={styles["community-mycourse-list-like"]}>
-    <img src={Like} alt="좋아요 아이콘" /> <span className={styles["community-mycourse-list-like-number"]}> {props.like}</span>
+    <img src={Like} alt="좋아요 아이콘" /> <span className={styles["community-mycourse-list-like-number"]}> {props.data.like}</span>
      </div>
-     <div className={styles["community-mycourse-list-nick"]}><span>{props.nick}</span></div>
-    <span className={styles["community-mycourse-list-date"]}>{props.reg_at}</span>
+     <div className={styles["community-mycourse-list-nick"]}><span>{props.data.nick}</span></div>
+    <span className={styles["community-mycourse-list-date"]}>{props.data.reg_at}</span>
   </a>
 );
 
