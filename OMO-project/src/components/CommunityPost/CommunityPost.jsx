d3d17dbec84dbb96fceb2c6d4b79ccd725b677f8
Sungchan CHo <kjyy07@naver.com>
2023-09-27 16:03:06 +0900

    [feat] 리뷰 작성 폼 만들기

--- OMO-project/src/components/CommunityPost/CommunityPost.jsx
+++ OMO-project/src/components/CommunityPost/CommunityPost.jsx
@@ -4,16 +4,45 @@ import Like from "../../assets/detail/empty-thumb.png";
 import Comment from "../../assets/community/worry-board/comment.png";
 import Submit from "../../assets/submit.png";
 import SubmitHover from "../../assets/submit-hover.png";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 
 export const CommunityPost = (props) => {
   const [showComments, setShowComments] = useState(false); // 초기에 숨김 상태
+
+  const [data, setData] = useState([]); // 댓글 리스트 초기 상태
+
+  const [content, setContent] = useState(""); // 댓글 내용
+
+  const contentInput = useRef(); //댓글 최소 입력시 포커스 주기위해 사용
+
+  const dataId = useRef(0); // 댓글 아이디
 
   // 댓글 달기 버튼 클릭 시 댓글창 표시/숨김 토글
   const toggleComments = () => {
     setShowComments(!showComments);
   };
 
+  // 댓글 최소 글자
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      alert("최소 1글자 이상 입력해주세요");
+      contentInput.current.focus();
+      return;
+    }
+    onCreate(content);
+  };
+
+  // 댓글 리스트에 댓글 추가
+  const onCreate = (content) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      content,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
   return (
     <>
       {/* 전체 영역 */}
@@ -72,22 +101,36 @@ export const CommunityPost = (props) => {
             {/* 댓글 입력창 */}
             <div className={styles["community-post-comment-input-container"]}>
               <img className={styles["community-post-comment-input-profile-img"]} src={props.src} alt="프로필 이미지" style={{width: "50px", height: "50px"}} />
-              <button className={styles["community-post-comment-input-button"]} type="submit">
+              <input
+                className={styles["community-post-comment-input"]}
+                type="text"
+                id="comment"
+                name="comment"
+                minLength="2"
+                maxLength="40"
+                size="10"
+                placeholder="댓글을 입력하세요..."
+                value={content}
+                ref={contentInput}
+                onChange={(e) => {
+                  setContent(e.target.value);
+                }}
+              ></input>
+              <button className={styles["community-post-comment-input-button"]} type="submit" onClick={handleSubmit}>
                 <img className={styles["community-post-comment-input-button-img"]} src={Submit} alt="제출 이미지" style={{width: "35px", height: "35px", marginTop: "6px"}} />
                 <img className={styles["community-post-comment-input-button-img-hover"]} src={SubmitHover} alt="제출 hover 이미지" style={{width: "35px", height: "35px"}} />
               </button>
-              <input className={styles["community-post-comment-input"]} type="text" id="comment" name="comment" minLength="2" maxLength="40" size="10" placeholder="댓글을 입력하세요..."></input>
             </div>
 
-            {/* 댓글 내용 */}
-            {props.comment_list.map((el) => (
+            {/* 댓글 리스트 내용 */}
+            {data.map((el) => (
               <ul className={styles["community-post-comment"]}>
                 <li>
-                  <img className={styles["community-post-comment-profile-img"]} src={el.src} alt="프로필 이미지" style={{width: "50px", height: "50px"}} />
+                  {/* <img className={styles["community-post-comment-profile-img"]} src={el.src} alt="프로필 이미지" style={{width: "50px", height: "50px"}} /> */}
                   <div className={styles["community-post-comment-box"]}>
                     <div className={styles["community-post-comment-nick-date"]}>
-                      <span className={styles["community-post-comment-box-nick"]}>{el.nick}</span>
-                      <span className={styles["community-post-comment-box-date"]}>{el.reg_at}</span>
+                      <span className={styles["community-post-comment-box-nick"]}>이니</span>
+                      <span className={styles["community-post-comment-box-date"]}>{new Date(el.created_date).toLocaleDateString()}</span>
                     </div>
                     <span className={styles["community-post-comment-box-content"]}>{el.content}</span>
                   </div>
