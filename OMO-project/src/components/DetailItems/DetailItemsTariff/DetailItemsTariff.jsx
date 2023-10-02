253e850f008e69b8c49586ecb0560936156269f6
hwanghaein <hhi9037@naver.com>
2023-10-02 13:27:11 +0900

    [feat] 상세페이지 3개에 리뷰 input창에 enter 기능, 제출 시 input 비우기 기능 추가

--- OMO-project/src/components/DetailItems/DetailItemsTariff/DetailItemsTariff.jsx
+++ OMO-project/src/components/DetailItems/DetailItemsTariff/DetailItemsTariff.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import {useState, useRef} from "react";
 import styles from "./DetailItemsTariff.module.css";
 import Jjim from "../../../assets/detail/empty-heart.png";
-import JjimClicked from "../../../assets/detail/red-heart.png"
+import JjimClicked from "../../../assets/detail/red-heart.png";
 import Like from "../../../assets/detail/empty-thumb.png";
-import LikeClicked from "../../../assets/detail/purple-thumb.png"
+import LikeClicked from "../../../assets/detail/purple-thumb.png";
 import Address from "../../../assets/detail/address.png";
 import SalesInfo from "../../../assets/detail/sales-info.png";
 import Menu from "../../../assets/detail/menu.png";
@@ -13,51 +13,87 @@ import ReviewIcon from "../../../assets/detail/review.png";
 import Submit from "../../../assets/submit.png";
 import SubmitHover from "../../../assets/submit-hover.png";
 import {Review} from "../Review/Review";
-// import { Map } from "./../../../components/Map/Map";
 import {priceTemplate} from "../../../utils/priceTemplate";
 import {reviewData} from "../../../const/reviewData";
 
 export const DetailItemsTariff = (props) => {
+  // 찜 버튼
+  const [item, setItem] = useState([]); // 상태변화함수, 빈배열로 시작
 
-    // 찜 버튼
-    const [imageSrcJjim, setImageSrcJjim] = useState(Jjim);
-    const [isClikedJjim, setIsClickedJjim] = useState(false);
-    const [countJjim,setCountJjim] = useState(0);
-  
-    const handleClickJjim =() =>{
-      if(isClikedJjim) {
-        setImageSrcJjim(Jjim);
-        setIsClickedJjim(false);
-        setCountJjim(count => count-1); 
-  
-      }else{
-        setImageSrcJjim(JjimClicked);
-        setIsClickedJjim(true);
-        setCountJjim(count => count+1);   
-      }
-    };
-  
-    // 좋아요 버튼
-    const [imageSrcLike, setImageSrcLike] = useState(Like);
-    const [isClikedLike, setIsClickedLike] = useState(false);
-    const [countLike,setCountLike] = useState(0);
-  
-    const handleClickLike =() =>{
-      if(isClikedLike) {
-        setImageSrcLike(Like);
-        setIsClickedLike(false);
-        setCountLike(countLike -1); 
-      }else{
-        setImageSrcLike(LikeClicked);
-        setIsClickedLike(true);
-        setCountLike(countLike +1);  
-      }
-    };
+  const [content, setContent] = useState(""); // 댓글 내용
 
+  const dataId = useRef(0); // id 인덱스 추가-> 변수처럼 사용 필요 -> useRef 사용
+
+  // 데이터 가져오기
   const {data} = props;
+
+  // 찜 버튼
+  const [imageSrcJjim, setImageSrcJjim] = useState(Jjim);
+  const [isClikedJjim, setIsClickedJjim] = useState(false);
+  const [countJjim, setCountJjim] = useState(0);
+
+  // 좋아요 버튼
+  const [imageSrcLike, setImageSrcLike] = useState(Like);
+  const [isClikedLike, setIsClickedLike] = useState(false);
+  const [countLike, setCountLike] = useState(0);
+
+  // onCreate 함수 (댓글 리스트에 댓글 추가)
+  const onCreate = (content) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      content,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setItem([newItem, ...item]);
+  };
+
+  // handleClickJjim 함수 (찜 버튼 - 색, 카운트)
+  const handleClickJjim = () => {
+    if (isClikedJjim) {
+      setImageSrcJjim(Jjim);
+      setIsClickedJjim(false);
+      setCountJjim((count) => count - 1);
+    } else {
+      setImageSrcJjim(JjimClicked);
+      setIsClickedJjim(true);
+      setCountJjim((count) => count + 1);
+    }
+  };
+
+  // handleClickLike 함수 (좋아요 버튼 - 색, 카운트)
+  const handleClickLike = () => {
+    if (isClikedLike) {
+      setImageSrcLike(Like);
+      setIsClickedLike(false);
+      setCountLike(countLike - 1);
+    } else {
+      setImageSrcLike(LikeClicked);
+      setIsClickedLike(true);
+      setCountLike(countLike + 1);
+    }
+  };
+
+  // handleSubmit 함수 (리뷰 제출 버튼)
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      alert("리뷰는 최소 1글자 이상 입력해주세요"); // 댓글 최소 글자
+      return;
+    }
+    onCreate(content); 
+    setContent('');
+  };
+
+      // handleOnKeyPress함수 (input에 적용할 Enter 키 입력 함수)
+      const handleOnKeyPress = (e) => {
+        if (e.key === "Enter") {
+          handleSubmit(); // Enter 입력이 되면 클릭 이벤트 실행
+        }
+      };
+
   return (
     <>
-      {/* 타이틀 */}
       <section className={styles["detail-title-container"]}>
         <div className={styles["detail-thumbnail-container"]}>
           <img src={data.thumbnail} alt="썸네일 이미지" />
@@ -65,14 +101,14 @@ export const DetailItemsTariff = (props) => {
         <span className={styles["detail-title"]}>{data.title}</span>
         <div className={styles["detail-like-jjim-container"]}>
           <div className={styles["detail-jjim"]}>
-          <button type="button" onClick={handleClickJjim}>
+            <button type="button" onClick={handleClickJjim}>
               <img src={imageSrcJjim} alt="찜 아이콘" style={{position: "absolute", top: "1px"}} />
             </button>
             <span className={styles["detail-jjim-number"]}> {countJjim}</span>
           </div>
           <span className={styles["detail-like-jjim-line"]}>|</span>
           <div className={styles["detail-like"]}>
-          <button type="button" onClick={handleClickLike}>
+            <button type="button" onClick={handleClickLike}>
               <img src={imageSrcLike} alt="좋아요 아이콘" style={{position: "absolute", top: "-1px"}} />
             </button>
             <span className={styles["detail-like-number"]}> {countLike}</span>
@@ -81,7 +117,6 @@ export const DetailItemsTariff = (props) => {
       </section>
 
       <div className={styles["detail-inner-container"]}>
-        {/* 주소 */}
         <section className={styles["detail-address-container"]}>
           <div className={styles["detail-address-inner-container"]}>
             <img src={Address} alt="주소 아이콘" style={{width: "20px", height: "25px", position: "absolute", top: "1px"}} />
@@ -91,7 +126,6 @@ export const DetailItemsTariff = (props) => {
           <span className={styles["detail-address-info-number"]}>{data.addressNumber}</span>
         </section>
 
-        {/* 영업정보 */}
         <section className={styles["detail-sales-info-container"]}>
           <div className={styles["detail-sales-info-inner-container"]}>
             <img src={SalesInfo} alt="영업정보 아이콘" style={{width: "25px", height: "25px", position: "absolute", top: "3px"}} />
@@ -108,22 +142,22 @@ export const DetailItemsTariff = (props) => {
           </div>
         </section>
 
-        {/* 메뉴 */}
         <section className={styles["detail-tariff-container"]}>
           <div className={styles["detail-tariff-inner-container"]}>
             <img src={Menu} alt="메뉴 아이콘" style={{width: "20px", height: "25px", position: "absolute", top: "1px"}} />
             <span className={styles["detail-tariff-title"]}>요금표</span>
           </div>
           {data.tariff.map((el) => (
+             <div key={el.id}>
             <ul className={styles["detail-tariff"]}>
               <li className={styles["detail-tariff-list"]}>
                 <span className={styles["detail-tariff-list-title"]}>{el.title}</span> <span className={styles["detail-tariff-list-price"]}>{priceTemplate(el.price)}원</span>
               </li>
             </ul>
+            </div>
           ))}
         </section>
 
-        {/* 전화 */}
         <section className={styles["detail-call-container"]}>
           <div className={styles["detail-call-inner-container"]}>
             <img src={Call} alt="전화 아이콘" style={{width: "25px", height: "25px", position: "absolute", top: "1px"}} />
@@ -132,15 +166,12 @@ export const DetailItemsTariff = (props) => {
           <span className={styles["detail-call"]}>{data.call}</span>
         </section>
 
-        {/* 구글맵 */}
         <section className={styles["detail-google-map-container"]}>
           <div className={styles["detail-google-map-inner-container"]}>
             <img src={data.googleMap} alt="구글맵 이미지" style={{width: "1000px", height: "300px"}} />
-            {/* <Map /> */}
           </div>
         </section>
 
-        {/* MBTI 별 통계 */}
         <section className={styles["detail-mbti-stats-container"]}>
           <div className={styles["detail-mbti-stats-inner-container"]}>
             <img src={Graph} alt="통계 아이콘" style={{width: "25px", height: "25px", position: "absolute", top: "1px"}} />
@@ -206,26 +237,37 @@ export const DetailItemsTariff = (props) => {
           </div>
         </section>
 
-        {/* 리뷰 */}
         <section className={styles["detail-review-container"]}>
           <div className={styles["detail-review-inner-container"]}>
             <img src={ReviewIcon} alt="리뷰 아이콘" style={{width: "25px", height: "25px", position: "absolute", top: "3px"}} />
             <span className={styles["detail-review-title"]}>리뷰</span>
 
             <div className={styles["detail-review-box-container"]}>
-              {/* input 박스 */}
               <div className={styles["detail-review-input-box"]}>
                 <img src={reviewData.find((item) => item.id).src} alt="프로필 이미지" style={{width: "50px", height: "50px"}} />
-                <button className={styles["detail-review-input-button"]} src={Submit} type="submit">
+                <button onClick={handleSubmit} className={styles["detail-review-input-button"]} src={Submit} type="submit">
                   <img className={styles["detail-review-input-button-img"]} src={Submit} alt="제출 이미지" style={{width: "35px", height: "35px"}} />
                   <img className={styles["detail-review-input-button-img-hover"]} src={SubmitHover} alt="제출 hover 이미지" style={{width: "35px", height: "35px"}} />
                 </button>
-                <input className={styles["detail-review-input"]} type="text" id="review" name="review" minlength="2" maxlength="40" size="10" placeholder="리뷰를 입력하세요..."></input>
+                <input
+                 onKeyDown={handleOnKeyPress}
+                  value={content || ""}
+                  onChange={(e) => {
+                    setContent(e.target.value);
+                  }}
+                  className={styles["detail-review-input"]}
+                  type="text"
+                  id="review"
+                  name="review"
+                  minLength="2"
+                  maxLength="40"
+                  size="10"
+                  placeholder="리뷰를 입력하세요..."
+                ></input>
               </div>
 
-              {/* 리뷰 박스 리스트 */}
-              {reviewData.map((review) => (
-                <Review data={review} />
+              {item.map((review) => (
+                <Review key={review.id} {...review} />
               ))}
             </div>
           </div>
