77a9f3a308eaee3562d4b2a9ba8c6caad76bdd4d
hwanghaein <hhi9037@naver.com>
2023-10-08 19:02:40 +0900

    [chore] 컴포넌트 제거 및 이동

--- OMO-project/src/components/MyCourse/MyCourseFindSearchModal/MyCourseFindSearchModal.jsx
+++ OMO-project/src/components/MyCourse/MyCourseFindSearchModal/MyCourseFindSearchModal.jsx
@@ -1,14 +1,27 @@
 import styles from "./MyCourseFindSearchModal.module.css";
 import {MypageListBox} from "./../../MypageListBox/MypageListBox";
 import {data} from "./../../../const/data";
-import MyCourseFindSearchBox from "./../MyCourseFindSearchBox/MyCourseFindSearchBox";
+import Magnifier from "./../../../assets/magnifier.png";
+import ModalClose from "./../../../assets/modal-close.png";
+
+
 
 const MyCourseFindSearchModal = () => (
   <>
     <div className={styles["Overlay"]}>
       <div className={styles["mycourse-find-search-modal-container"]}>
         <label className={styles["mycourse-find-search-modal-title"]} htmlFor="find-search">
-          <MyCourseFindSearchBox />
+        <div className={styles["mycourse-find-search-container"]}>
+    <div className={styles["mycourse-find-search-img-container"]}>
+      <a href="#">
+        <img src={Magnifier} alt="돋보기" />
+      </a>
+    </div>
+    <button className={styles["mycourse-find-search-close-btn"]} type="button">
+      <img className={styles["mycourse-find-search-close-btn-img"]} src={ModalClose} alt="닫기 아이콘" />
+    </button>
+    <input className={styles["mycourse-find-search-input-container"]} placeholder="검색어를 입력해주세요" maxLength={20} />
+  </div>
         </label>
         <div className={styles["mycourse-find-search-modal-list-box-container"]}>
           {data.map((el) => {
