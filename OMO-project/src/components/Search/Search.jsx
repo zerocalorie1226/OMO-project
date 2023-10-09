52b36cbe1c455427a91d2272c6ac69b07953733c
hwanghaein <hhi9037@naver.com>
2023-10-10 00:49:48 +0900

    [style] 스타일 수정

--- OMO-project/src/components/Search/Search.jsx
+++ OMO-project/src/components/Search/Search.jsx
@@ -5,9 +5,9 @@ import Magnifier from "../../assets/magnifier.png";
 const Search = () => (
   <div className={styles["main-search-container"]}>
     <div className={styles["main-search-img-container"]}>
-      <img src={LogoCircle} alt="로고 동그라미" />
+      <img  className={styles["main-search-logocircle"]} src={LogoCircle} alt="로고 동그라미" />
       <a href="#">
-        <img src={Magnifier} alt="돋보기" />
+        <img className={styles["main-search-magnifier"]} src={Magnifier} alt="돋보기" />
       </a>
     </div>
     <input className={styles["main-search-input-container"]} placeholder="장소를 입력해주세요" />
