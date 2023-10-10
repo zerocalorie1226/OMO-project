37e20cbc438531ab65abd93d0af60a5cfa36b1e9
hwanghaein <hhi9037@naver.com>
2023-10-10 19:34:18 +0900

    [style] 마이페이지 스타일 수정

--- OMO-project/src/components/MypageFilter/MypageFilter.jsx
+++ OMO-project/src/components/MypageFilter/MypageFilter.jsx
@@ -5,15 +5,15 @@ const MypageFilter = () => (
     <button type="button" className={styles["filter-total"]}>
       전체
     </button>
-    <p className={styles["filter-p"]}> | </p>
+    <p className={styles["filter-bar"]}> | </p>
     <button type="button" className={styles["filter-free"]}>
       자유 게시판
     </button>
-    <p className={styles["filter-p"]}> | </p>
+    <p className={styles["filter-bar"]}> | </p>
     <button type="button" className={styles["filter-worry"]}>
       고민 게시판
     </button>
-    <p className={styles["filter-p"]}> | </p>
+    <p className={styles["filter-bar"]}> | </p>
     <button type="button" className={styles["filter-question"]}>
       문의 게시판
     </button>
