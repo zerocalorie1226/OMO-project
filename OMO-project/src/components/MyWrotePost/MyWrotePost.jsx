a268fc7dee75936956e0dbbf611e86c6742f24d1
Sungchan CHo <kjyy07@naver.com>
2023-10-18 18:03:18 +0900

    [feat] 마이 페이지 내가 쓴 글 클릭시 열리게 만들기

--- OMO-project/src/components/MyWrotePost/MyWrotePost.jsx
+++ OMO-project/src/components/MyWrotePost/MyWrotePost.jsx
@@ -40,46 +40,18 @@ export const MyWrotePost = (props) => {
 
           {/* 글 내용 */}
           <span className={styles["my-wrote-post-content"]}>{props.content}</span>
-
-          {/*공감수*/}
-          <div className={styles["my-wrote-post-number-report-wapper"]}>
-            <span className={styles["my-wrote-post-like-number"]}>좋아요 {props.like}</span>
-            <span className={styles["my-wrote-post-view-number"]}>• 조회수 {props.view}</span>
-            <span className={styles["my-wrote-post-comment-number"]}>• 댓글 {props.comment}</span>
-
-            {/* 신고 아이콘 */}
-            <button className={styles["my-wrote-post-report-button"]} type="button">
-              <img className={styles["my-wrote-post-report"]} alt="신고 아이콘" src={Report} style={{width: "32px", height: "32px"}} />
-            </button>
-          </div>
         </div>
 
         <div className={styles["my-wrote-post-button-wrapper"]}>
-          {/*좋아요 버튼*/}
-          <button type="button" className={styles["my-wrote-post-like-button"]}>
-            <img className={styles["my-wrote-post-like-button-img"]} src={Like} style={{width: "25px", height: "25px"}} />
-            좋아요
-          </button>
-
           {/* 댓글달기 버튼 */}
           <button type="button" className={styles["my-wrote-post-comment-button"]}>
             <img className={styles["my-wrote-post-comment-button-img"]} src={Comment} style={{width: "25px", height: "25px"}} />
-            댓글 달기
+            댓글
           </button>
         </div>
 
         {/* 하단 댓글창 전체박스 */}
         <div className={styles["my-wrote-post-comment-container"]}>
-          {/* 댓글 입력창 */}
-          <div className={styles["my-wrote-post-comment-input-container"]}>
-            <img className={styles["my-wrote-post-comment-input-profile-img"]} src={props.src} alt="프로필 이미지" style={{width: "50px", height: "50px"}} />
-            <button className={styles["my-wrote-post-comment-input-button"]} type="submit">
-              <img className={styles["my-wrote-post-comment-input-button-img"]} src={Submit} alt="제출 이미지" style={{width: "35px", height: "35px"}} />
-              <img className={styles["my-wrote-post-comment-input-button-img-hover"]} src={SubmitHover} alt="제출 hover 이미지" style={{width: "35px", height: "35px"}} />
-            </button>
-            <input className={styles["my-wrote-post-comment-input"]} type="text" id="comment" name="comment" minlength="2" maxlength="40" size="10" placeholder="댓글을 입력하세요..."></input>
-          </div>
-
           {/* 댓글 내용 */}
           {props.comment_list.map((el) => (
             <ul className={styles["my-wrote-post-comment"]}>
