6e9a080deae6e447d67d18024c61a5c61deb0374
Sungchan CHo <kjyy07@naver.com>
2023-10-18 17:12:37 +0900

    [feat] 나만의 코스 삭제 버튼 누르면 컴포넌트 삭제하기

--- OMO-project/src/components/MyCourse/MyCourseFindBox/MyCourseFindBox.jsx
+++ OMO-project/src/components/MyCourse/MyCourseFindBox/MyCourseFindBox.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, {useState} from "react";
 import styles from "./MyCourseFindBox.module.css";
 import downArrow from "../../../assets/my-course/write/down-arrow.png";
 import Delete from "../Button/Delete/Delete";
@@ -9,34 +9,52 @@ import MyCourseFindRecentModal from "../MyCourseFindRecentModal/MyCourseFindRece
 import MyCourseFindSearchModal from "../MyCourseFindSearchModal/MyCourseFindSearchModal";
 
 const MyCourseFindBox = () => {
-  
   const [interestModal, setInterestModal] = useState(false);
   const [recentModal, setRecentModal] = useState(false);
   const [searchModal, setSearchModal] = useState(false);
+  const [isFindBoxVisible, setFindBoxVisible] = useState(true); // Add this state
+
+  const handleDeleteClick = () => {
+    setFindBoxVisible(false);
+  };
+
   return (
-    <div className={styles["mycourse-find-box-total-container"]}>
-      <MyCourseCalendar />
+    <>
+      {isFindBoxVisible && (
+        <div className={styles["mycourse-find-box-total-container"]}>
+          <MyCourseCalendar />
 
-      <div className={styles["mycourse-find-box-button-container"]}>
-        <FindButton text={"관심 목록에서 찾기"} onClick={() => { setInterestModal(true) }} />
-        {interestModal ?  <MyCourseFindInterestModal interestModal={interestModal} setInterestModal={setInterestModal} /> : null }
-       
-        <FindButton text={"최근 본 목록에서 찾기"} onClick={() =>{ setRecentModal(true) }} />
-        {recentModal ?  <MyCourseFindRecentModal recentModal={recentModal} setRecentModal={setRecentModal} /> : null }
-        
-        <FindButton text={"검색을 통해 찾기"} onClick={() => { setSearchModal(true) }} />
-        {searchModal ?  <MyCourseFindSearchModal searchModal={searchModal} setSearchModal={setSearchModal} /> : null }
-      </div>
-      <img src={downArrow} alt="아래 화살표" className={styles["mycourse-find-box-down-arrow-img"]} />
-    </div>
+          <Delete onClick={handleDeleteClick} />
+          <div className={styles["mycourse-find-box-button-container"]}>
+            <FindButton
+              text={"관심 목록에서 찾기"}
+              onClick={() => {
+                setInterestModal(true);
+              }}
+            />
+            {interestModal ? <MyCourseFindInterestModal interestModal={interestModal} setInterestModal={setInterestModal} /> : null}
 
-    // <MyCourseFindInterestModal/>
-    // <MyCourseFindRecentModal/>
-    // <MyCourseFindSearchModal/>
+            <FindButton
+              text={"최근 본 목록에서 찾기"}
+              onClick={() => {
+                setRecentModal(true);
+              }}
+            />
+            {recentModal ? <MyCourseFindRecentModal recentModal={recentModal} setRecentModal={setRecentModal} /> : null}
+
+            <FindButton
+              text={"검색을 통해 찾기"}
+              onClick={() => {
+                setSearchModal(true);
+              }}
+            />
+            {searchModal ? <MyCourseFindSearchModal searchModal={searchModal} setSearchModal={setSearchModal} /> : null}
+          </div>
+
+          <img src={downArrow} alt="아래 화살표" className={styles["mycourse-find-box-down-arrow-img"]} />
+        </div>
+      )}
+    </>
   );
 };
 export default MyCourseFindBox;
-
-// interset
-// recent
-// search
