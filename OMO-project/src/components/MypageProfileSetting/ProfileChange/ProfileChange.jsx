c82f83ac071bbbc98153badc9190482f39c88ef1
hwanghaein <hhi9037@naver.com>
2023-10-18 23:25:51 +0900

    [feat] 연필 안보이는 현상 빼고는 삭제까지 완료

--- OMO-project/src/components/MypageProfileSetting/ProfileChange/ProfileChange.jsx
+++ OMO-project/src/components/MypageProfileSetting/ProfileChange/ProfileChange.jsx
@@ -3,56 +3,62 @@ import DeleteImg from "../../../assets/my-page/setting/profile-delete.png";
 import {useRef, useState} from "react";
 import DefaultImg from "../../../assets/my-page/setting/default-background.png";
 
-
 const ProfileChange = () => {
-  
-const [Image, setImage] = useState(DefaultImg);
-const [File, setFile] = useState("");
-const fileInput = useRef(null);
+  const [Image, setImage] = useState(DefaultImg);
+  const [File, setFile] = useState(""); 
 
-const onChange = (e) => {
-  if (e.target.files[0]) {
-    setFile(e.target.files[0]);
-  } else {
-    //업로드 취소할 시
-    setImage(DefaultImg);
-    return;
-  }
+  const fileInput = useRef(null);
 
-  //화면에 프로필 사진 표시
-  const reader = new FileReader();
-  reader.onload = () => {
-    if (reader.readyState === 2) {
-      setImage(reader.result);
+
+  const onChange = (e) => {
+    if (e.target.files[0]) {
+      setFile(e.target.files[0]);
+    } else {
+      //업로드 취소할 시
+      setImage(DefaultImg);
+      return;
     }
+    //화면에 프로필 사진 표시
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
   };
-  reader.readAsDataURL(e.target.files[0]);
-};
-  return(
-  <div className={styles["profile-setting-main-profile-change-container"]}>
-    <p className={styles["profile-setting-main-profile-change-title"]}>프로필 변경</p>
-    <div className={styles["profile-setting-main-profile-change-img-box"]}>
-      <img src={Image} alt="프로필 사진" className={styles["profile-setting-main-profile-change-img"]} />
-      <label className={styles["profile-setting-main-profile-change-add-img"]} htmlFor="input-file">
-        <span className={styles["profile-setting-main-profile-change-add-img-icon"]}>블로그 이미지 찾아보기</span>
-        <input
-          type="file"
-          accept="image/*"
-          id="input-file"
-          className={styles["profile-setting-main-profile-change-add-img-input"]}
+  return (
+    <div className={styles["profile-setting-main-profile-change-container"]}>
+      <p className={styles["profile-setting-main-profile-change-title"]}>프로필 변경</p>
+      <div className={styles["profile-setting-main-profile-change-img-box"]}>
+        <img src={Image} alt="프로필 사진" className={styles["profile-setting-main-profile-change-img"]} />
+        <label className={styles["profile-setting-main-profile-change-add-img"]} htmlFor="input-file">
+          <span className={styles["profile-setting-main-profile-change-add-img-icon"]}>블로그 이미지 찾아보기</span>
+          <input
+            type="file"
+            accept="image/*"
+            id="input-file"
+            className={styles["profile-setting-main-profile-change-add-img-input"]}
+            onClick={() => {
+              fileInput.current.value = null;
+              fileInput.current.click();
+            }}
+            ref={fileInput}
+            onChange={onChange}
+          />
+        </label>
+        <button
+          type="button"
+          className={styles["profile-setting-main-profile-change-delete"]}
           onClick={() => {
-            fileInput.current.click();
+            setImage(DefaultImg);
           }}
-          ref={fileInput}
-          onChange={onChange}
-        />
-      </label>
-      <button type="button" className={styles["profile-setting-main-profile-change-delete"]}>
-        <img src={DeleteImg} alt="이미지 삭제" className={styles["profile-setting-main-profile-change-delete-img"]} />
-      </button>
+        >
+          {Image === DefaultImg ? null : <img src={DeleteImg} alt="이미지 삭제" className={styles["profile-setting-main-profile-change-delete-img"]} />}
+        </button>
+      </div>
     </div>
-  </div>
-);
-        };
+  );
+};
 
 export default ProfileChange;
