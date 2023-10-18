e981ff1dbf4966674ff071fa4701d97fa3f397f7
hwanghaein <hhi9037@naver.com>
2023-10-18 16:43:00 +0900

    [style] 마이페이지 프로필 설정에서 프로필 변경 마크업

--- OMO-project/src/components/MypageProfileSetting/ProfileChange/ProfileChange.jsx
+++ OMO-project/src/components/MypageProfileSetting/ProfileChange/ProfileChange.jsx
@@ -1,14 +1,19 @@
 import styles from "./ProfileChange.module.css";
-import ImgChange from "../../../assets/my-page/setting/white-pencil.png";
-import ProfileImg from "../../../assets/profile-img.jpg";
+import AddImg from "../../../assets/my-page/setting/profile-add.png";
+import DeleteImg from "../../../assets/my-page/setting/profile-delete.png";
+// import ProfileImg from "../../../assets/profile-img.jpg";
 
 const ProfileChange = () => (
   <div className={styles["profile-setting-main-profile-change-container"]}>
     <p className={styles["profile-setting-main-profile-change-title"]}>프로필 변경</p>
-    <img src={ProfileImg} alt="프로필 사진" className={styles["profile-setting-main-profile-change-img"]} />
-    <a className={styles["profile-setting-main-profile-change-background"]}></a>
-    <img src={ImgChange} alt="이미지 변경" className={styles["profile-setting-main-profile-change-change-img"]} />
-    <a href="#" className={styles["profile-setting-main-profile-change-click"]}></a>
+    <div className={styles["profile-setting-main-profile-change-img-box"]}>
+    {/* <img src={ProfileImg} alt="프로필 사진" className={styles["profile-setting-main-profile-change-img"]} /> */}
+    <label className={styles["profile-setting-main-profile-change-add-img"]} for="input-file">
+      <span className={styles["profile-setting-main-profile-change-add-img-icon"]}>블로그 이미지 찾아보기</span>
+      <input type="file" accept="image/*" id="input-file" className={styles["profile-setting-main-profile-change-add-img-input"]}/> 
+      </label>
+  <button type="button" className={styles["profile-setting-main-profile-change-delete"]}><img src={DeleteImg} alt="이미지 삭제" className={styles["profile-setting-main-profile-change-delete-img"]} /></button>
+    </div>
   </div>
 );
 
