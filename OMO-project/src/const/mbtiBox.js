91d16630227564ed8d992b5c3b282eae402f94af
hwanghaein <hhi9037@naver.com>
2023-10-25 21:27:54 +0900

    [feat] 커뮤니티 나만의 코스 게시판 mbti 필터 컴포넌트 및 상태 분리

---
+++ OMO-project/src/const/mbtiBox.js
@@ -0,0 +1,31 @@
+import LightPurpleI from "../assets/community/my-course-board/I-light-purple.png";
+import LightPurpleE from "../assets/community/my-course-board/E-light-purple.png";
+import LightPurpleJ from "../assets/community/my-course-board/J-light-purple.png";
+import LightPurpleP from "../assets/community/my-course-board/P-light-purple.png";
+import DeepPurpleI from "../assets/community/my-course-board/I-deep-purple.png";
+import DeepPurpleE from "../assets/community/my-course-board/E-deep-purple.png";
+import DeepPurpleJ from "../assets/community/my-course-board/J-deep-purple.png";
+import DeepPurpleP from "../assets/community/my-course-board/P-deep-purple.png";
+
+export const mbtiBox = [
+  {
+    id: 1,
+    light_purple: LightPurpleI,
+    deep_purple: DeepPurpleI,
+  },
+  {
+    id: 2,
+    light_purple: LightPurpleE,
+    deep_purple: DeepPurpleE,
+  },
+  {
+    id: 3,
+    light_purple: LightPurpleJ,
+    deep_purple: DeepPurpleJ,
+  },
+  {
+    id: 4,
+    light_purple: LightPurpleP,
+    deep_purple: DeepPurpleP,
+  },
+];
