30faacfe881599c313912d2feeac79948717cf6d
Sungchan CHo <kjyy07@naver.com>
2023-10-30 15:39:00 +0900

    [feat] 나만의 코스 편집 페이지 삭제, 저장 버튼 클릭 시 alert 창 추가

--- OMO-project/src/components/MyCourse/Button/Save/Save.jsx
+++ OMO-project/src/components/MyCourse/Button/Save/Save.jsx
@@ -1,11 +1,19 @@
+import React, {useState} from "react";
 import styles from "./Save.module.css";
+import {Link} from "react-router-dom";
 
-const Save = () => (
-  <div className={styles["save-button-container"]}>
-    <button type="button" className={styles["save-button"]}>
-      저장
-    </button>
-  </div>
-);
-
+const Save = () => {
+  const saveClick = () => {
+    alert("저장되었습니다.");
+  };
+  return (
+    <div className={styles["save-button-container"]}>
+      <Link to="/MyCourseMyVersion" onClick={saveClick}>
+        <button type="button" className={styles["save-button"]}>
+          저장
+        </button>
+      </Link>
+    </div>
+  );
+};
 export default Save;
