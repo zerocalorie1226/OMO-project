3b8b4269e8c5ad82817000662ee8164c69d6ef42
hwanghaein <hhi9037@naver.com>
2023-11-07 21:43:33 +0900

    [feat] 고민게시판 글쓰기 기능 추가

--- OMO-project/src/components/CommunityWorryPostList/CommunityWorryPostList.jsx
+++ OMO-project/src/components/CommunityWorryPostList/CommunityWorryPostList.jsx
@@ -1,20 +1,14 @@
 import styles from "./CommunityWorryPostList.module.css";
-import {communityWorryPost} from "../../const/communityWorryPost"; //고민게시판 데이터
-import { CommunityPostItem } from "../CommunityPostItem/CommunityPostItem";
+import {CommunityPostItem} from "../CommunityPostItem/CommunityPostItem";
 
-export const CommunityWorryPostList = () => {
-
-  return(
+export const CommunityWorryPostList = ({communityWorryPostList}) => {
+  return (
     <>
-    <section className={styles["community-worry-container"]}>
-    {communityWorryPost.map((el) => {
-      return (
-        <CommunityPostItem
-          {...el}
-        />
-      );
-    })}
-  </section>
-</>
+      <section className={styles["community-worry-container"]}>
+        {communityWorryPostList.map((el) => {
+          return <CommunityPostItem key={el.id} {...el} />;
+        })}
+      </section>
+    </>
   );
-}
+};
--- OMO-project/src/pages/Community/WorryBoard/WorryBoard.jsx
+++ OMO-project/src/pages/Community/WorryBoard/WorryBoard.jsx
@@ -1,20 +1,36 @@
+import {useRef, useState} from "react";
 import styles from "./WorryBoard.module.css";
 import {communityPageFilter} from "./../../../const/communityPageFilter";
 import {CommunityCategory} from "./../../../components/CommunityCategory/CommunityCategory";
 import Filter from "../../../components/Filter/Filter";
 import ListSearch from "./../../../components/ListSearch/ListSearch";
 import {ScrollToTop} from "../../../components/ScrollToTop/ScrollToTop";
-import {WritingButton} from "../../../components/WritingButton/WritingButton";
-import {Link} from "react-router-dom";
-import { CommunityWorryPostList } from "../../../components/CommunityWorryPostList/CommunityWorryPostList";
+import {CommunityWorryPostList} from "../../../components/CommunityWorryPostList/CommunityWorryPostList";
+import WritingButtonImg from "../../../assets/writing-button.png";
+import WriteWorryBoard from "../../../components/WriteWorryBoard/WriteWorryBoard";
 
 const WorryBoard = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [data, setData] = useState([]);
 
- 
-  return(
-  <>
-    {/* 카테고리 */}
-    <CommunityCategory />
+  const dataId = useRef(0);
+
+  const onCreate = (title, content) => {
+    const reg_at = new Date().getTime();
+    const newItem = {
+      title,
+      content,
+      reg_at,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  return (
+    <>
+      {/* 카테고리 */}
+      <CommunityCategory />
 
       {/* 필터 + 검색창 */}
       <div className={styles["community-component-container"]}>
@@ -27,15 +43,24 @@ const WorryBoard = () => {
       </div>
 
       {/* 게시글 리스트 */}
-      <CommunityWorryPostList />
+      <CommunityWorryPostList communityWorryPostList={data} />
 
       {/* 스크롤 */}
       <ScrollToTop />
 
       {/* 글쓰기 */}
-      <Link to="/WriteBoard">
-        <WritingButton />
-      </Link>
+      <div className={styles["writing-btn-container"]}>
+        <button
+          type="button"
+          className={styles["writing-btn"]}
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          <img src={WritingButtonImg} alt="글쓰기 아이콘" style={{width: "80px", height: "80px"}} />{" "}
+        </button>
+        {openModal ? <WriteWorryBoard onCreate={onCreate} openModal={openModal} setOpenModal={setOpenModal} /> : null}
+      </div>
     </>
   );
 };
