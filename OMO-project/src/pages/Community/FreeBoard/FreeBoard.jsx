338281927360ee2d332dc5258279df510a515a64
hwanghaein <hhi9037@naver.com>
2023-11-26 14:52:44 +0900

    [feat] 자유게시판 localStorage 기능 구현

--- OMO-project/src/pages/Community/FreeBoard/FreeBoard.jsx
+++ OMO-project/src/pages/Community/FreeBoard/FreeBoard.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import React, {useEffect, useRef, useState, useReducer} from "react";
 import styles from "./FreeBoard.module.css";
 import {communityPageFilter} from "./../../../const/communityPageFilter"; //필터 데이터
 import {CommunityCategory} from "./../../../components/CommunityCategory/CommunityCategory"; //카테고리
@@ -9,26 +9,69 @@ import {CommunityFreePostList} from "../../../components/CommunityFreePostList/C
 import WritingButtonImg from "../../../assets/writing-button.png";
 import WriteFreeBoard from "../../../components/WritePost/WriteFreeBoard/WriteFreeBoard";
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    default:
+      return state;
+  }
+
+  localStorage.setItem("freeboard", JSON.stringify(newState));
+  return newState;
+};
+
+export const BoardStateContext = React.createContext();
+export const BoardDispatchContext = React.createContext();
+
+
 const FreeBoard = () => {
+
+  const [data, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("freeboard");
+    if (localData) {
+      const boardList = JSON.parse(localData).sort((a, b) => parseInt(b.id) - parseInt(a.id));
+
+      if (boardList.length >= 1) {
+        dataId.current = parseInt(boardList[0].id) + 1;
+        dispatch({type: "INIT", data: boardList});
+      }
+    }
+  }, []);
+
   const [openModal, setOpenModal] = useState(false);
-  const [data, setData] = useState([]);
 
   const dataId = useRef(0);
 
-  const onCreate = (title, content) => {
-    const reg_at = new Date().getTime();
-    const newItem = {
+ // CREATE
+ const onCreate = (title, content) => {
+  dispatch({
+    type: "CREATE",
+    data: {
+      id: dataId.current,
+      reg_at: new Date().getTime(),
       title,
       content,
-      reg_at,
-      id: dataId.current,
-    };
-    dataId.current += 1;
-    setData([newItem, ...data]);
-  };
-
+    },
+  });
+  dataId.current += 1;
+};
   return (
     <>
+          <BoardStateContext.Provider value={data}>
+        <BoardDispatchContext.Provider
+          value={{
+            onCreate,
+          }}
+        >
       {/* 카테고리 */}
       <CommunityCategory />
 
@@ -62,6 +105,8 @@ const FreeBoard = () => {
         </button>
         {openModal ? <WriteFreeBoard onCreate={onCreate} openModal={openModal} setOpenModal={setOpenModal} /> : null}
       </div>
+      </BoardDispatchContext.Provider>
+      </BoardStateContext.Provider>
     </>
   );
 };
