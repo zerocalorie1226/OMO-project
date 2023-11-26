3edcb5d46dd33f578df4484e3ba0958745e5e6ff
hwanghaein <hhi9037@naver.com>
2023-11-26 14:52:54 +0900

    [feat] qna게시판 localStorage 기능 구현

--- OMO-project/src/pages/Community/InquiryBoardQnA/InquiryBoardQnA.jsx
+++ OMO-project/src/pages/Community/InquiryBoardQnA/InquiryBoardQnA.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import React, {useEffect, useRef, useState, useReducer} from "react";
 import styles from "./InquiryBoardQnA.module.css";
 import CommunityInquiryFilter from "../../../components/CommunityInquiryFilter/CommunityInquiryFilter";
 import {CommunityQnAPostList} from "../../../components/CommunityQnAPostList/CommunityQnAPostList";
@@ -8,51 +8,92 @@ import {ScrollToTop} from "../../../components/ScrollToTop/ScrollToTop";
 import WritingButtonImg from "../../../assets/writing-button.png";
 import WriteQnABoard from "../../../components/WritePost/WriteQnABoard/WriteQnABoard";
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    default:
+      return state;
+  }
+
+  localStorage.setItem("qnaboard", JSON.stringify(newState));
+  return newState;
+};
+
+export const BoardStateContext = React.createContext();
+export const BoardDispatchContext = React.createContext();
 
 const InquiryBoardQnA = () => {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("qnaboard");
+    if (localData) {
+      const boardList = JSON.parse(localData).sort((a, b) => parseInt(b.id) - parseInt(a.id));
+
+      if (boardList.length >= 1) {
+        dataId.current = parseInt(boardList[0].id) + 1;
+        dispatch({type: "INIT", data: boardList});
+      }
+    }
+  }, []);
+
   const [openModal, setOpenModal] = useState(false);
-  const [data, setData] = useState([]);
 
   const dataId = useRef(0);
 
+  // CREATE
   const onCreate = (title, content) => {
-    const reg_at = new Date().getTime();
-    const newItem = {
-      title,
-      content,
-      reg_at,
-      id: dataId.current,
-    };
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        reg_at: new Date().getTime(),
+        title,
+        content,
+      },
+    });
     dataId.current += 1;
-    setData([newItem, ...data]);
   };
 
-  
-  return(
-  
-  <div>
-    <CommunityCategory />
-    <div className={styles["inquiry-board-qna-filter-search-container"]}>
-      <CommunityInquiryFilter />
-      <ListSearch />
-    </div>
-    <hr className={styles["inquiry-board-qna-hr"]} />
-    <CommunityQnAPostList communityQnAPostList={data} />
-    <ScrollToTop />
-      <div className={styles["writing-btn-container"]}>
-        <button
-          type="button"
-          className={styles["writing-btn"]}
-          onClick={() => {
-            setOpenModal(true);
+  return (
+    <>
+      <BoardStateContext.Provider value={data}>
+        <BoardDispatchContext.Provider
+          value={{
+            onCreate,
           }}
         >
-          <img src={WritingButtonImg} alt="글쓰기 아이콘" style={{width: "80px", height: "80px"}} />{" "}
-        </button>
-        {openModal ? <WriteQnABoard onCreate={onCreate} openModal={openModal} setOpenModal={setOpenModal} /> : null}
-      </div>
-  </div>
-);
-  };
+          <CommunityCategory />
+          <div className={styles["inquiry-board-qna-filter-search-container"]}>
+            <CommunityInquiryFilter />
+            <ListSearch />
+          </div>
+          <hr className={styles["inquiry-board-qna-hr"]} />
+          <CommunityQnAPostList communityQnAPostList={data} />
+          <ScrollToTop />
+          <div className={styles["writing-btn-container"]}>
+            <button
+              type="button"
+              className={styles["writing-btn"]}
+              onClick={() => {
+                setOpenModal(true);
+              }}
+            >
+              <img src={WritingButtonImg} alt="글쓰기 아이콘" style={{width: "80px", height: "80px"}} />{" "}
+            </button>
+            {openModal ? <WriteQnABoard onCreate={onCreate} openModal={openModal} setOpenModal={setOpenModal} /> : null}
+          </div>
+        </BoardDispatchContext.Provider>
+      </BoardStateContext.Provider>
+    </>
+  );
+};
 
 export default InquiryBoardQnA;
