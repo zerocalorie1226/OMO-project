693954adb19113ca4b1629c9863afd41fd6185bf
hwanghaein <hhi9037@naver.com>
2023-11-26 14:15:04 +0900

    [chore] 고민게시판 localStorage 기능 구현

--- OMO-project/src/pages/Community/WorryBoard/WorryBoard.jsx
+++ OMO-project/src/pages/Community/WorryBoard/WorryBoard.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import React, {useEffect, useRef, useState, useReducer} from "react";
 import styles from "./WorryBoard.module.css";
 import {communityPageFilter} from "./../../../const/communityPageFilter";
 import {CommunityCategory} from "./../../../components/CommunityCategory/CommunityCategory";
@@ -9,58 +9,102 @@ import {CommunityWorryPostList} from "../../../components/CommunityWorryPostList
 import WritingButtonImg from "../../../assets/writing-button.png";
 import WriteWorryBoard from "../../../components/WritePost/WriteWorryBoard/WriteWorryBoard";
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    default:
+      return state;
+  }
+
+  localStorage.setItem("board", JSON.stringify(newState));
+  return newState;
+};
+
+export const BoardStateContext = React.createContext();
+export const BoardDispatchContext = React.createContext();
+
 const WorryBoard = () => {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("board");
+    if (localData) {
+      const boardList = JSON.parse(localData).sort((a, b) => parseInt(b.id) - parseInt(a.id));
+
+      if (boardList.length >= 1) {
+        dataId.current = parseInt(boardList[0].id) + 1;
+        dispatch({type: "INIT", data: boardList});
+      }
+    }
+  }, []);
+
   const [openModal, setOpenModal] = useState(false);
-  const [data, setData] = useState([]);
 
   const dataId = useRef(0);
 
+  // CREATE
   const onCreate = (title, content) => {
-    const reg_at = new Date().getTime();
-    const newItem = {
-      title,
-      content,
-      reg_at,
-      id: dataId.current,
-    };
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        reg_at: new Date().getTime(),
+        title,
+        content,
+      },
+    });
     dataId.current += 1;
-    setData([newItem, ...data]);
   };
 
   return (
     <>
-      {/* 카테고리 */}
-      <CommunityCategory />
-
-      {/* 필터 + 검색창 */}
-      <div className={styles["community-component-container"]}>
-        <div className={styles["community-filter-container"]}>
-          {communityPageFilter.map((el) => {
-            return <Filter key={el.id} {...el} />;
-          })}
-        </div>
-        <ListSearch />
-      </div>
-
-      {/* 게시글 리스트 */}
-      <CommunityWorryPostList communityWorryPostList={data} />
-
-      {/* 스크롤 */}
-      <ScrollToTop />
-
-      {/* 글쓰기 */}
-      <div className={styles["writing-btn-container"]}>
-        <button
-          type="button"
-          className={styles["writing-btn"]}
-          onClick={() => {
-            setOpenModal(true);
+      <BoardStateContext.Provider value={data}>
+        <BoardDispatchContext.Provider
+          value={{
+            onCreate,
           }}
         >
-          <img src={WritingButtonImg} alt="글쓰기 아이콘" style={{width: "80px", height: "80px"}} />{" "}
-        </button>
-        {openModal ? <WriteWorryBoard onCreate={onCreate} openModal={openModal} setOpenModal={setOpenModal} /> : null}
-      </div>
+          {/* 카테고리 */}
+          <CommunityCategory />
+
+          {/* 필터 + 검색창 */}
+          <div className={styles["community-component-container"]}>
+            <div className={styles["community-filter-container"]}>
+              {communityPageFilter.map((el) => {
+                return <Filter key={el.id} {...el} />;
+              })}
+            </div>
+            <ListSearch />
+          </div>
+
+          {/* 게시글 리스트 */}
+          <CommunityWorryPostList communityWorryPostList={data} />
+
+          {/* 스크롤 */}
+          <ScrollToTop />
+
+          {/* 글쓰기 */}
+          <div className={styles["writing-btn-container"]}>
+            <button
+              type="button"
+              className={styles["writing-btn"]}
+              onClick={() => {
+                setOpenModal(true);
+              }}
+            >
+              <img src={WritingButtonImg} alt="글쓰기 아이콘" style={{width: "80px", height: "80px"}} />{" "}
+            </button>
+            {openModal ? <WriteWorryBoard onCreate={onCreate} openModal={openModal} setOpenModal={setOpenModal} /> : null}
+          </div>
+        </BoardDispatchContext.Provider>
+      </BoardStateContext.Provider>
     </>
   );
 };
