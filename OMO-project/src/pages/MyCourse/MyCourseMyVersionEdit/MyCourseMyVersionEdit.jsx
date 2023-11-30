9de20897527f2a9c047c5c1fb017b3f30959ef8f
hwanghaein <hhi9037@naver.com>
2023-11-30 20:03:13 +0900

    [feat] 나만의 코스 글쓰기 및 편집페이지 수정

--- OMO-project/src/pages/MyCourse/MyCourseMyVersionEdit/MyCourseMyVersionEdit.jsx
+++ OMO-project/src/pages/MyCourse/MyCourseMyVersionEdit/MyCourseMyVersionEdit.jsx
@@ -1,34 +1,21 @@
 import styles from "./MyCourseMyVersionEdit.module.css";
-import MyCoursePlusBox from "../../../components/MyCourse/MyCoursePlusBox/MyCoursePlusBox";
+import pencil from "../../../assets/my-course/write/gray-pencil.png";
+import MyCourseFindBox from "./../../../components/MyCourse/MyCourseFindBox/MyCourseFindBox";
+import MyCourseAfter from "../../../components/MyCourse/MyCourseAfter/MyCourseAfter";
 import {ScrollToTop} from "../../../components/ScrollToTop/ScrollToTop";
 import Save from "../../../components/MyCourse/Button/Save/Save";
-import {data} from "../../../const/data";
-import MyCourseDataBox from "../../../components/MyCourse/MyCourseDataBox/MyCourseDataBox";
-import MyCourseCalendar from './../../../components/MyCourse/MyCourseCalendar/MyCourseCalendar';
+
 
 const MyCourseMyVersionEdit = () => {
-  const myCourseMyVersionEdit1 = data.find((item) => item.id === 1);
-  const myCourseMyVersionEdit2 = data.find((item) => item.id === 2);
-  const myCourseMyVersionEdit7 = data.find((item) => item.id === 7);
   return (
-    <div className={styles["mycourse-myversion-edit-total-container"]}>
-      <div className={styles["mycourse-myversion-edit-title-container"]}>
-        <span className={styles["mycourse-myversion-edit-title"]}>킹부엉의 인천 맛집 탐방</span>
-      </div>
-      <div className={styles["mycourse-myversion-edit-calendar-container"]}><MyCourseCalendar/></div>
-      <MyCourseDataBox
-        key={myCourseMyVersionEdit1.id} {...myCourseMyVersionEdit1}
-      />
-      <div className={styles["mycourse-myversion-edit-calendar-container"]}><MyCourseCalendar/></div>
-      <MyCourseDataBox
-        key={myCourseMyVersionEdit2.id} {...myCourseMyVersionEdit2}
-      />
-      <div className={styles["mycourse-myversion-edit-calendar-container"]}><MyCourseCalendar/></div>
-      <MyCourseDataBox
-        key={myCourseMyVersionEdit7.id} {...myCourseMyVersionEdit3}
-      />
 
-      <MyCoursePlusBox />
+    <div className={styles["mycourse-write-total-container"]}>
+      <div className={styles["mycourse-write-title-container"]}>
+        <input type="text" placeholder="나만의 코스" maxLength="10" className={styles["mycourse-write-input-title"]} />
+        <img src={pencil} alt="연필" className={styles["mycourse-write-img-title"]} />
+      </div>
+      <MyCourseFindBox />
+      <MyCourseAfter />
       <Save />
       <ScrollToTop />
     </div>
