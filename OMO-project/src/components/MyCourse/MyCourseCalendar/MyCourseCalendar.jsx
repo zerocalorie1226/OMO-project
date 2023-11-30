801b3f35d7edebee372f29817ff1b20e8e4aaeff
hwanghaein <hhi9037@naver.com>
2023-11-30 18:46:07 +0900

    [feat] 나만의 코스 달력에서 초기에 현재 시간으로 설정되는 기능 추가

--- OMO-project/src/components/MyCourse/MyCourseCalendar/MyCourseCalendar.jsx
+++ OMO-project/src/components/MyCourse/MyCourseCalendar/MyCourseCalendar.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import styles from "./MyCourseCalendar.module.css";
 
-const MyCourseCalendar = () => (
-  <>
-    <input
-      className={styles["mycourse-time-container"]}
-      type="datetime-local"
-      id="meeting-time"
-      name="meeting-time"
-      defaultValue="2023-04-15T19:30"
-      min="2023-04-15T00:00"
-      max="2023-04-16T00:00"
-    ></input>
-  </>
-);
+
+const getStringDate= (date)=>{
+  return date.toISOString().slice(0,16);
+}
+
+
+
+const MyCourseCalendar = () => {
+  
+  const [date, setDate] = useState(getStringDate(new Date()));
+  
+  return (
+    <>
+      <input
+        className={styles["mycourse-time-container"]}
+        type="datetime-local"
+        id="meeting-time"
+        name="meeting-time"
+        value={date}
+        onChange={(e)=>setDate(e.target.value)}
+        // defaultValue="2023-04-15T19:30"
+        // min="2023-04-15T00:00"
+        // max="2023-04-16T00:00"
+      ></input>
+    </>
+  );
+};
 
 export default MyCourseCalendar;
-
-
