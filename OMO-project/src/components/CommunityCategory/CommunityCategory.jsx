fc36b5ce3df8d207847e3878ab0601df617e26fd
Sungchan CHo <kjyy07@naver.com>
2023-12-13 15:25:19 +0900

    [feat] 커뮤니티 카테고리 클릭 시 배경 변환

--- OMO-project/src/components/CommunityCategory/CommunityCategory.jsx
+++ OMO-project/src/components/CommunityCategory/CommunityCategory.jsx
@@ -1,26 +1,35 @@
 import styles from "./CommunityCategory.module.css";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
-export const CommunityCategory = () => (
-  <>
-    <div className={styles["community-category-container"]}>
-      <div className={styles["community-category-title-container"]}>
-        <Link to="/MyCourseBoard" className={styles["community-category-mycourse"]}>
-          나만의 코스 게시판
-        </Link>
+export const CommunityCategory = () => {
+  const location = useLocation();
 
-        <Link to="/WorryBoard" className={styles["community-category-worry"]}>
-          고민 게시판
-        </Link>
+  const isMyCourseActive = location.pathname === "/MyCourseBoard";
+  const isWorryActive = location.pathname === "/WorryBoard";
+  const isFreeActive = location.pathname === "/FreeBoard";
+  const isInquiryActive = location.pathname === "/InquiryBoardFrequent";
 
-        <Link to="/FreeBoard" className={styles["community-category-free"]}>
-          자유 게시판
-        </Link>
+  return (
+    <>
+      <div className={styles["community-category-container"]}>
+        <div className={styles["community-category-title-container"]}>
+          <Link to="/MyCourseBoard" className={`${styles["community-category-mycourse"]} ${isMyCourseActive ? styles["community-category-link-active"] : ""}`}>
+            나만의 코스 게시판
+          </Link>
 
-        <Link to="/InquiryBoardFrequent" className={styles["community-category-inquiry"]}>
-          문의 게시판
-        </Link>
+          <Link to="/WorryBoard" className={`${styles["community-category-worry"]} ${isWorryActive ? styles["community-category-link-active"] : ""}`}>
+            고민 게시판
+          </Link>
+
+          <Link to="/FreeBoard" className={`${styles["community-category-free"]} ${isFreeActive ? styles["community-category-link-active"] : ""}`}>
+            자유 게시판
+          </Link>
+
+          <Link to="/InquiryBoardFrequent" className={`${styles["community-category-inquiry"]} ${isInquiryActive ? styles["community-category-link-active"] : ""}`}>
+            문의 게시판
+          </Link>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
