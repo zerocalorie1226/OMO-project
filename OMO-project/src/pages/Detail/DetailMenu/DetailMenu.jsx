24761e4cf7ef97745dbb6b3906c707c0d171f0b5
hwanghaein <hhi9037@naver.com>
2023-12-19 17:47:30 +0900

    [feat] 상세 페이지 router 설정

--- OMO-project/src/pages/Detail/DetailMenu/DetailMenu.jsx
+++ OMO-project/src/pages/Detail/DetailMenu/DetailMenu.jsx
@@ -2,13 +2,24 @@ import styles from "./DetailMenu.module.css";
 import {data} from "../../../const/data";
 import {DetailItemsMenu} from "../../../components/DetailItems/DetailItemsMenu/DetailItemsMenu";
 import {ScrollToTop} from "../../../components/ScrollToTop/ScrollToTop";
+import {  useParams } from 'react-router-dom';
 
 const DetailMenu = () => {
-  const storeItem = data.find((item) => item.id === 1);
+
+  const {id} = useParams();
+
+
+
+  const targetData = data.find(
+    (it) => parseInt(it.id) === parseInt(id)
+  );
+
+
+
   return (
     <>
       <section className={styles["detail-container"]}>
-        <DetailItemsMenu key={storeItem.id} data={storeItem} />
+        <DetailItemsMenu data={targetData} />
       </section>
       <ScrollToTop />
     </>
