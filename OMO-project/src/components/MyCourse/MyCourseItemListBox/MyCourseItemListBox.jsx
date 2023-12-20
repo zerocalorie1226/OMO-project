eeefd4d7af00392b78a34b57fb932e9fbe7ff657
hwanghaein <hhi9037@naver.com>
2023-12-20 21:47:52 +0900

    [style] 나만의 코스 리스트 박스 스타일 수정

--- OMO-project/src/components/MyCourse/MyCourseItemListBox/MyCourseItemListBox.jsx
+++ OMO-project/src/components/MyCourse/MyCourseItemListBox/MyCourseItemListBox.jsx
@@ -1,6 +1,6 @@
 import styles from "./MyCourseItemListBox.module.css";
-import Like from "../../../assets/my-page/interest/empty-thumb.png";
-import Jjim from "../../../assets/my-page/interest/empty-heart.png";
+import Like from "../../../assets/detail/purple-thumb.png";
+import Jjim from "../../../assets/detail/red-heart.png";
 
 export const MyCourseItemListBox = ({state, setState, el, onClick}) => {
   return (
@@ -18,10 +18,10 @@ export const MyCourseItemListBox = ({state, setState, el, onClick}) => {
 
         <div className={styles["mycourse-list-box-like-jjim-container"]}>
           <div className={styles["mycourse-list-box-jjim"]}>
-            <img src={Jjim} alt="찜 아이콘" /> <span className={styles["mycourse-list-box-jjim-number"]}> {el.jjim}</span>
+            <img src={Jjim} alt="찜 아이콘" style={{position: "absolute", top: "0px"}} /> <span className={styles["mycourse-list-box-jjim-number"]}> {el.jjim}</span>
           </div>
           <div className={styles["mycourse-list-box-like"]}>
-            <img src={Like} alt="좋아요 아이콘" /> <span className={styles["mycourse-list-box-like-number"]}> {el.like}</span>
+            <img src={Like} alt="좋아요 아이콘" style={{position: "absolute", top: "-1px"}}  /> <span className={styles["mycourse-list-box-like-number"]}> {el.like}</span>
           </div>
         </div>
 
@@ -29,6 +29,7 @@ export const MyCourseItemListBox = ({state, setState, el, onClick}) => {
         <span className={styles["mycourse-list-box-address-brief"]}>{el.addressBrief}</span>
         <img className={styles["mycourse-list-box-img1"]} src={el.src1} />
         <img className={styles["mycourse-list-box-img2"]} src={el.src2} />
+        <img className={styles["mycourse-list-box-img3"]} src={el.src3} />
       </div>
     </>
   );
