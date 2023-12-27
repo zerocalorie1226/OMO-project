f0d708fcb87e422aad282ef6d28460316fc8a141
Sungchan CHo <kjyy07@naver.com>
2023-12-27 13:26:42 +0900

    [feat] 리스트페이지 검색 기능 추가

--- OMO-project/src/components/ListSearch/ListSearch.jsx
+++ OMO-project/src/components/ListSearch/ListSearch.jsx
@@ -1,15 +1,27 @@
+import React, {useState} from "react";
 import styles from "./ListSearch.module.css";
 import Magnifier from "../../assets/magnifier.png";
 
-const ListSearch = () => (
-  <div className={styles["list-search-container"]}>
-    <div className={styles["list-search-img-container"]}>
-      <a href="#">
-        <img src={Magnifier} alt="돋보기" />
-      </a>
+const ListSearch = ({onSearch}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+
+    onSearch(term);
+  };
+
+  return (
+    <div className={styles["list-search-container"]}>
+      <div className={styles["list-search-img-container"]}>
+        <a href="#">
+          <img src={Magnifier} alt="돋보기" />
+        </a>
+      </div>
+      <input className={styles["list-search-input-container"]} placeholder="검색어를 입력해 주세요." maxLength={20} value={searchTerm} onChange={handleInputChange} />
     </div>
-    <input className={styles["list-search-input-container"]} placeholder="검색어를 입력해 주세요." maxLength={20} />
-  </div>
-);
+  );
+};
 
 export default ListSearch;
