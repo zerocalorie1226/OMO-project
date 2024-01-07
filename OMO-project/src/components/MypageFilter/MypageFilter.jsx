41ce9d0521cebdd6d41a71b18ad57636c6b94edd
hwanghaein <hhi9037@naver.com>
2024-01-07 16:34:20 +0900

    [chore] 주석 수정

--- OMO-project/src/components/MypageFilter/MypageFilter.jsx
+++ OMO-project/src/components/MypageFilter/MypageFilter.jsx
@@ -5,19 +5,19 @@ const MypageFilter = ({setSelectedCategory, selectedCategory}) => {
 
 return(
   <div className={styles["filter-container"]}>
-    {/* <button type="button"  className={`${styles["filter-total"]} ${selectedCategory==="all" ? styles["filter-total-active"] : ""}`} onClick={() => setSelectedCategory("all")}>
+    {/* <button type="button"  className={`${styles["filter-total"]} ${selectedCategory==="all" ? styles["filter-active"] : ""}`} onClick={() => setSelectedCategory("all")}>
       전체
     </button>
     <p className={styles["filter-bar"]}> | </p> */}
-    <button type="button"  className={`${styles["filter-free"]} ${selectedCategory==="free" ? styles["filter-free-active"] : ""}`} onClick={() => setSelectedCategory("free")}>
+    <button type="button"  className={`${styles["filter-free"]} ${selectedCategory==="free" ? styles["filter-active"] : ""}`} onClick={() => setSelectedCategory("free")}>
       자유게시판
     </button>
     <p className={styles["filter-bar"]}> | </p>
-    <button type="button"  className={`${styles["filter-worry"]} ${selectedCategory==="worry" ? styles["filter-worry-active"] : ""}`} onClick={() => setSelectedCategory("worry")}>
+    <button type="button"  className={`${styles["filter-worry"]} ${selectedCategory==="worry" ? styles["filter-active"] : ""}`} onClick={() => setSelectedCategory("worry")}>
       고민게시판
     </button>
     <p className={styles["filter-bar"]}> | </p>
-    <button type="button"  className={`${styles["filter-qna"]} ${selectedCategory==="qna" ? styles["filter-qna-active"] : ""}`} onClick={() => setSelectedCategory("qna")}>
+    <button type="button"  className={`${styles["filter-qna"]} ${selectedCategory==="qna" ? styles["filter-active"] : ""}`} onClick={() => setSelectedCategory("qna")}>
       문의게시판
     </button>
   </div>
