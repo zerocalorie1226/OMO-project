d5dfdcfe757ddbb01e3b32a3cb8ec394b9b82e98
hwanghaein <hhi9037@naver.com>
2024-01-10 17:17:45 +0900

    [style] 헤더에 회원가입 추가

--- OMO-project/src/components/Header/Header.jsx
+++ OMO-project/src/components/Header/Header.jsx
@@ -6,34 +6,47 @@ export const Header = () => (
   <>
     <header>
       <div className={styles["main-header-container"]}>
-        <ul>
-          <li>
-            <Link to="/">
-              <img src={MainLogo} alt="메인 로고" />
-            </Link>
-          </li>
-          <div className={styles["main-header-menu-container"]}>
-          <li>
+        <div>
+          <Link to="/">
+            <img src={MainLogo} alt="메인 로고" />
+          </Link>
+        </div>
+
+<div className={styles["main-header-menu-container"]}>
+
+        <div className={styles["main-header-menu-left-container"]}>
+          <div>
             <Link to="/MyCourseBoard">커뮤니티</Link>
-          </li>
-          <li>
-            <Link to="/MyCourseMain">나만의 코스</Link>
-          </li>
-          <li>
-            <Link to="/Notice">공지사항</Link>
-          </li>
           </div>
-          <li>
-            <Link to="/Login" className={styles["main-header-login"]}>
-              로그인
-            </Link>
-          </li>
-          <li>
-            <Link to="/MyInfo" className={styles["main-header-mypage"]}>
-              마이 페이지
-            </Link>
-          </li>
-        </ul>
+          <div>
+            <Link to="/MyCourseMain">나만의 코스</Link>
+          </div>
+          <div>
+            <Link to="/Notice">공지사항</Link>
+          </div>
+        </div>
+
+
+        <div className={styles["main-header-menu-right-container"]}>
+        <div>
+          <Link to="/SignUp" className={styles["main-header-signup"]}>
+            회원가입
+          </Link>
+        </div>
+        <div>
+          <Link to="/Login" className={styles["main-header-login"]}>
+            로그인
+          </Link>
+        </div>
+        <div>
+          <Link to="/MyInfo" className={styles["main-header-mypage"]}>
+            마이 페이지
+          </Link>
+        </div>
+        </div>
+
+        </div>
+
       </div>
       <hr />
     </header>
