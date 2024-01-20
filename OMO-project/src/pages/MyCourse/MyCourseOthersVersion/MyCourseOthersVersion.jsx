cbe5a43479bb804059e3c2c450e192cef978b2ae
hwanghaein <hhi9037@naver.com>
2024-01-20 17:03:39 +0900

    [feat] 나만의 코스 다른 사람 상세 페이지 일부 마크업 및 리스트 router 설정

--- OMO-project/src/pages/MyCourse/MyCourseOthersVersion/MyCourseOthersVersion.jsx
+++ OMO-project/src/pages/MyCourse/MyCourseOthersVersion/MyCourseOthersVersion.jsx
@@ -1,43 +1,78 @@
 import styles from "./MyCourseOthersVersion.module.css";
 import {ScrollToTop} from "../../../components/ScrollToTop/ScrollToTop";
 import {data} from "../../../const/data";
-import MyCourseDataBox from "../../../components/MyCourse/MyCourseDataBox/MyCourseDataBox";
 import ComeBackList from "../../../components/MyCourse/Button/ComeBackList/ComeBackList";
 import MyCourseCalendar from "./../../../components/MyCourse/MyCourseCalendar/MyCourseCalendar";
 import {Link} from "react-router-dom";
+import {MyCourseOthersDataBox} from "./../../../components/MyCourseOthersDataBox/MyCourseOthersDataBox";
+import { communityMyCourse } from "../../../const/communityMyCourse";
+import { useParams } from "react-router-dom";
 
 const MyCourseOthersVersion = () => {
-  const MyCourseOthersVersion1 = data.find((item) => item.id === 1);
-  const MyCourseOthersVersion2 = data.find((item) => item.id === 2);
-  const MyCourseOthersVersion8 = data.find((item) => item.id === 8);
+  const {id} = useParams();
+
+  const targetData = communityMyCourse.find(  
+    (it) => parseInt(it.id) === parseInt(id)
+  );
+  console.log(communityMyCourse);
+  console.log(data);
+  const MyCourseOthersDataBox1 = data.find((item) => item.id === 1);
+  const MyCourseOthersDataBox2 = data.find((item) => item.id === 2);
+  const MyCourseOthersDataBox3 = data.find((item) => item.id === 3);
+  
   return (
-    <div className={styles["mycourse-othersversion-total-container"]}>
-      <div className={styles["mycourse-othersversion-title-container"]}>
-        <span className={styles["mycourse-othersversion-title"]}>킹부엉의 인천 맛집 탐방</span>
+    <div className={styles["mycourse-detail-total-container"]}>
+      <div className={styles["mycourse-detail-title-container"]}>
+        <span>{targetData.title}</span>
       </div>
-      <div className={styles["mycourse-othersversion-calendar-container"]}>
-        <MyCourseCalendar />
+      <div className={styles["mycourse-detail-course-container"]}>
+        <div className={styles["mycourse-detail-course-item-container"]}>
+          <div className={styles["mycourse-detail-calendar-container"]}>2024년 1월 20일 오후 12:00</div>
+          <MyCourseOthersDataBox key={MyCourseOthersDataBox1.id} {...MyCourseOthersDataBox1} />
+        </div>
+        <div className={styles["mycourse-detail-course-item-container"]}>
+          <div className={styles["mycourse-detail-calendar-container"]}>2024년 1월 20일 오후 12:00</div>
+          <MyCourseOthersDataBox key={MyCourseOthersDataBox2.id} {...MyCourseOthersDataBox2} />
+        </div>
+        <div className={styles["mycourse-detail-course-item-container"]}>
+          <div className={styles["mycourse-detail-calendar-container"]}>2024년 1월 20일 오후 12:00</div>
+          <MyCourseOthersDataBox key={MyCourseOthersDataBox3.id} {...MyCourseOthersDataBox3} />
+        </div>
       </div>
-      <MyCourseDataBox
-        key={MyCourseOthersVersion1.id} {...MyCourseOthersVersion1}
-      />
-      <div className={styles["mycourse-othersversion-calendar-container"]}>
-        <MyCourseCalendar />
+      <div className={styles["mycourse-detail-edit-share-button-container"]}>
       </div>
-      <MyCourseDataBox
-        key={MyCourseOthersVersion2.id} {...MyCourseOthersVersion2}
-      />
-      <div className={styles["mycourse-othersversion-calendar-container"]}>
-        <MyCourseCalendar />
-      </div>
-      <MyCourseDataBox
-        key={MyCourseOthersVersion8.id} {...MyCourseOthersVersion8}
-      />
-      <Link to="/MyCourseBoard">
-        <ComeBackList />
-      </Link>
+       <Link to="/MyCourseBoard">
+      <ComeBackList />
+    </Link>
       <ScrollToTop />
     </div>
+
+    // < className={styles["mycourse-othersversion-total-container"]}>
+    //   <div className={styles["mycourse-othersversion-title-container"]}>
+    //     <span className={styles["mycourse-othersversion-title"]}>킹부엉의 인천 맛집 탐방</span>
+    //   </div>
+    //   <div className={styles["mycourse-othersversion-calendar-container"]}>
+    //     <MyCourseCalendar />
+    //   </div>
+    // <MyCourseOthersDataBox
+    //   key={MyCourseOthersVersion1.id} {...MyCourseOthersVersion1}
+    // />
+    //   <div className={styles["mycourse-othersversion-calendar-container"]}>
+    //     <MyCourseCalendar />
+    //   </div>
+    //   <MyCourseOthersDataBox
+    //     key={MyCourseOthersDataBox2.id} {...MyCourseOthersDataBox2}
+    //   />
+    //   <div className={styles["mycourse-othersversion-calendar-container"]}>
+    //     <MyCourseCalendar />
+    //   </div>
+    //   <MyCourseOthersDataBox
+    //     key={MyCourseOthersVersion8.id} {...MyCourseOthersVersion8}
+    //   />
+    // <Link to="/MyCourseBoard">
+    //   <ComeBackList />
+    // </Link>
+    // <ScrollToTop />
   );
 };
 
