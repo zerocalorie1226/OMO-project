8ca1eb3559de26d51450b386095f6d0ae766ba5d
hwanghaein <hhi9037@naver.com>
2024-02-08 03:49:22 +0900

    [feat] 나만의 코스 글 작성 내역 없음 렌더링 추가

--- OMO-project/src/pages/MyCourse/MyCourseMain/MyCourseMain.jsx
+++ OMO-project/src/pages/MyCourse/MyCourseMain/MyCourseMain.jsx
@@ -1,34 +1,29 @@
 // 나만의 코스 메인페이지 (Home)
-import { useContext, useEffect, useState } from "react";
-import MyCourseList from "../../../components/MyCourse/MyCourseList/MyCourseList";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { MyCourseStateContext } from "../../../App";
 import { ScrollToTop } from "../../../components/ScrollToTop/ScrollToTop";
 import { WritingButton } from "../../../components/WritingButton/WritingButton";
+import MyCourseList from "../../../components/MyCourse/MyCourseList/MyCourseList";
 import styles from "./MyCourseMain.module.css";
-import { Link, useSearchParams } from "react-router-dom";
-import { MyCourseStateContext } from "../../../App";
 
 const MyCourseMain = () => {
   const myCourseList = useContext(MyCourseStateContext);
-  // const [curDate] = useState(new Date());
-  // const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
-
-  // useEffect(() => {
-  //   console.log("메인:", myCourseList);
-  // }, [myCourseList, curDate]);
 
   return (
     <div className={styles["mycourse-container"]}>
       <h2 className={styles["mycourse-title"]}>나만의 코스</h2>
 
-      <div className={styles["mycourse-header-container"]}>
-        {/* increaseMonth와 decreaseMonth 관련 코드 삭제 */}
-        {/* <div className={styles["mycourse-header-date"]}>{headText}</div> */}
-        {/* increaseMonth와 decreaseMonth 관련 코드 삭제 */}
-      </div>
-
-      <div className={styles["mycourse-list-container"]}>
-        <MyCourseList myCourseList={myCourseList} /> {/* 데이터 필터링 삭제 */}
-      </div>
+      {myCourseList.length === 0 ? (
+        <div className={styles["no-boardlist"]}>
+          글 작성 내역이 없습니다. 우측 하단에 있는 글쓰기 버튼을 통해 게시글을 작성해주세요.
+        </div>
+      ) : (
+        <div className={styles["mycourse-list-container"]}>
+          <MyCourseList myCourseList={myCourseList} />
+        </div>
+      )}
+      
       <ScrollToTop />
       <Link to="/MyCourseNewWrite">
         <WritingButton />
