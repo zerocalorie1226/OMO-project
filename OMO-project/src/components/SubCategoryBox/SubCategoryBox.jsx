86730e8b44cb98ffea09c40b7d0613265d63cd66
조성찬 <kjyy07@naver.com>
2024-02-13 14:36:32 +0900

    [fix] 메인페이지-서브페이지 오류 수정

--- OMO-project/src/components/SubCategoryBox/SubCategoryBox.jsx
+++ OMO-project/src/components/SubCategoryBox/SubCategoryBox.jsx
@@ -4,12 +4,12 @@ import {Link} from "react-router-dom";
 export const SubCategoryBox = (props) => (
   <>
     <Link to="/List">
-      <a href="#" className={styles["sub-category-container"]}>
+      
         <div className={styles["sub-category-box"]}>
           <span className={styles["sub-category-box-title"]}>{props.title}</span>
           <img className={styles["sub-category-box-img"]} src={props.src} />
         </div>
-      </a>
+
     </Link>
   </>
 );
