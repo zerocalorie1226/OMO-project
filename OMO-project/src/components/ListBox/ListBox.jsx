74e72708898ae7ece7bf8579356277d1cb29f04b
hwanghaein <hhi9037@naver.com>
2024-02-27 17:44:34 +0900

    [style] ListBox에 intro 빠진 것 다시 추가

--- OMO-project/src/components/ListBox/ListBox.jsx
+++ OMO-project/src/components/ListBox/ListBox.jsx
@@ -33,7 +33,7 @@ export const ListBox = (props,) => {
           </div>
 
           <span className={`${styles["list-box-runtime"]} ${props.runTime === "영업 준비 중" ? styles["ready"] : ""} `}> {props.runTime}</span>
-
+          <span className={styles["list-box-intro"]}>{props.intro}</span>
           <span className={styles["list-box-address-brief"]}>{props.addressBrief}</span>
           <img className={styles["list-box-img1"]} src={props.src1} />
           <img className={styles["list-box-img2"]} src={props.src2} />
