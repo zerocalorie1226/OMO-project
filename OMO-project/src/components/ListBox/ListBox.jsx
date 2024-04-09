bda54c9a8d14117d8634bae1d077bb87ada346f8
황혜인 <hhi9037@naver.com>
2024-04-09 19:46:36 +0900

    [style] ListBox 스타일 수정

--- OMO-project/src/components/ListBox/ListBox.jsx
+++ OMO-project/src/components/ListBox/ListBox.jsx
@@ -35,9 +35,9 @@ export const ListBox = (props) => {
           {/* <span className={`${styles["list-box-runtime"]} ${props.runTime === "영업 준비 중" ? styles["ready"] : ""} `}> {props.runTime}</span> */}
           {/* <span className={styles["list-box-intro"]}>{props.intro}</span> */}
           <span className={styles["list-box-address-brief"]}>{props.address_name}</span>
+          {/* <img className={styles["list-box-img1"]} src={props.src1} /> */}
           <img className={styles["list-box-img1"]} src={props.src1} />
           <img className={styles["list-box-img2"]} src={props.src2} />
-          <img className={styles["list-box-img3"]} src={props.src3} />
         </div>
       </button>
     </>
