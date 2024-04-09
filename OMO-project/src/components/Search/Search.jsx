e5c2bf64c932bf7cd69be5859df9f22778862877
조성찬 <kjyy07@naver.com>
2024-04-09 15:58:41 +0900

    [feat] 주소 API 통신

--- OMO-project/src/components/Search/Search.jsx
+++ OMO-project/src/components/Search/Search.jsx
@@ -4,8 +4,8 @@ import Magnifier from "../../assets/magnifier.png";
 import Location from "../../assets/main/location.png";
 import { useState } from "react";
 
-const Search = ({onSearch}) => {
-  const [query, setQuery] = useState('');
+const Search = ({onSearch,query,setQuery}) => {
+
 
   const handleInputChange = (event) => {  // input 입력값 받음
     setQuery(event.target.value);
@@ -37,7 +37,7 @@ const Search = ({onSearch}) => {
       </form>
       <div className={styles["main-search-location-container"]}>
         <img src={Location} alt="위치" />
-        <div className={styles["main-search-location-current"]}>현재 설정된 위치 : 마포구 </div>
+        <div className={styles["main-search-location-current"]}>현재 설정된 위치 : {query} </div>
       </div>
     </div>
   );
--- OMO-project/src/pages/main/Main.jsx
+++ OMO-project/src/pages/main/Main.jsx
@@ -8,22 +8,20 @@ import axios from "axios";
 
 const Main = () => {
   const [searchResults, setSearchResults] = useState([]);
+  const [query, setQuery] = useState('');
 
   // 여기서 서버로 검색 쿼리를 전송하고 결과를 받아오는 로직을 구현
   // 결과를 setSearchResults를 통해 저장
   const handleSearch = async (query) => {
     try {
       // 주소 뭐 써야하는지 모르겠음. 수정 필요
-      const response = await axios.get(`https://api.oneulmohae.co.kr/list/한식?page=1`, {
+      const response = await axios.get(`https://api.oneulmohae.co.kr/local/${query}`, {
         params: {
           q: query
         },
-        headers: {
-          x: 126.647861,
-          y: 37.443504
-        }
       });
       setSearchResults(response.data);
+      console.log(response)
     } catch (error) {
       console.error("검색 중 오류 발생:", error);
     }
@@ -31,7 +29,7 @@ const Main = () => {
 
   return (
     <>
-      <Search onSearch={handleSearch} />
+      <Search onSearch={handleSearch} query={query} setQuery={setQuery} />
       <Weather />
       <div className={styles["main-category-container"]}>
         {main.map((el) => {
