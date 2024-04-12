add6aa76f01f06e1efe519a786ea699648e3e5d5
황혜인 <hhi9037@naver.com>
2024-04-12 22:13:57 +0900

    [feat] 리스트 페이지 검색창 기능 fix

--- OMO-project/src/pages/list/List.jsx
+++ OMO-project/src/pages/list/List.jsx
@@ -1,30 +1,17 @@
-import React, { useState ,useEffect} from "react";
-import { useParams } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
 import styles from "./List.module.css";
 import ListSearch from "../../components/ListSearch/ListSearch";
-import { ListBox } from "../../components/ListBox/ListBox";
-import { ScrollToTop } from "../../components/ScrollToTop/ScrollToTop";
+import {ListBox} from "../../components/ListBox/ListBox";
+import {ScrollToTop} from "../../components/ScrollToTop/ScrollToTop";
 import axios from "axios";
 
-const List = ({ recentData, setRecentData, dataCopy,searchResultsX,searchResultsY }) => {
+const List = ({recentData, setRecentData, dataCopy, searchResultsX, searchResultsY}) => {
   // useParams를 사용하여 category 값을 가져옵니다.
-  const { category: categoryParam } = useParams();
-  
+  const {category: categoryParam} = useParams();
+
   // URL에서 받아온 category 값이 없는 경우 'all'로 간주합니다.
-  const category = categoryParam || 'all';
-  
-  const [searchTerm, setSearchTerm] = useState("");
-
-  // 카테고리와 검색어 모두를 고려하여 데이터 필터링
-  const filteredData = dataCopy.filter((item) => {
-    const matchesCategory = category === 'all' || item.category.includes(category);
-    const matchesSearchTerm = item.title.toLowerCase().includes(searchTerm.toLowerCase());
-    return matchesCategory && matchesSearchTerm;
-  });
-
-  const onSearch = (term) => {
-    setSearchTerm(term); // 검색어 상태 업데이트
-  };
+  const category = categoryParam || "all";
 
   // 아이템을 최근 본 목록에 추가하는 함수
   const addRecentItem = (item) => {
@@ -37,26 +24,22 @@ const List = ({ recentData, setRecentData, dataCopy,searchResultsX,searchResults
       runTime: item.runTime,
       src1: item.src1,
       src2: item.src2,
-      src3: item.src3
+      src3: item.src3,
     };
-  
+
     setRecentData((prevData) => {
       const existingIndex = prevData.findIndex((recentItem) => recentItem.id === item.id);
       let updatedData = [...prevData];
-      if (existingIndex === -1) { // 아이템이 최근 본 목록에 없는 경우 추가
+      if (existingIndex === -1) {
+        // 아이템이 최근 본 목록에 없는 경우 추가
         updatedData.unshift(newItem);
       }
       return updatedData;
     });
   };
 
-  console.log(searchResultsX)
-  console.log(searchResultsY)
-  console.log(category)
-
   const [listData, setListData] = useState(null);
 
-  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -76,34 +59,42 @@ const List = ({ recentData, setRecentData, dataCopy,searchResultsX,searchResults
     fetchData();
   }, []); // category가 변경될 때마다 데이터를 새로 가져옵니다.
 
-console.log(listData);
+  // console.log("리스트데이터: ",listData);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredData = listData
+    ? listData.filter((item) => {
+        return item.place_name && item.place_name.toLowerCase().includes(searchTerm.toLowerCase());
+      })
+    : [];
+
+  const onSearch = (term) => {
+    setSearchTerm(term); // 검색어 상태 업데이트
+  };
 
   return (
     <>
-          {listData === null ? (
-  <div>Loading...</div>
-) : (
-  <div>
-    <div className={styles["list-component-container"]}>
-      <ListSearch searchTerm={searchTerm} onSearch={onSearch} />
-    </div>
-    <section className={styles["list-list-container"]}>
-      {listData && listData.length > 0 ? ( // listData가 null 또는 undefined인지 확인
-        <div className={styles["list-list-box-container"]}>
-          {listData.map((item) => (
-            <ListBox
-              key={item.id}
-              {...item}
-              addRecentItem={() => addRecentItem(item)}
-            />
-          ))}
-        </div>
+      {filteredData === null ? (
+        <div>Loading...</div>
       ) : (
-        <span className={styles["list-no-search-result"]}>검색 결과가 없습니다.</span>
+        <div>
+          <div className={styles["list-component-container"]}>
+            <ListSearch searchTerm={searchTerm} onSearch={onSearch} />
+          </div>
+          <section className={styles["list-list-container"]}>
+            {filteredData && filteredData.length > 0 ? ( // filteredData가 null 또는 undefined인지 확인
+              <div className={styles["list-list-box-container"]}>
+                {filteredData.map((item) => (
+                  <ListBox key={item.id} {...item} addRecentItem={() => addRecentItem(item)} />
+                ))}
+              </div>
+            ) : (
+              <span className={styles["list-no-search-result"]}>검색 결과가 없습니다.</span>
+            )}
+          </section>
+        </div>
       )}
-    </section>
-  </div>
-)}
       <ScrollToTop />
     </>
   );
