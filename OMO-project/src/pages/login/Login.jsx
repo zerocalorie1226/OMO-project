322d35a36f4a0d5822781b7f5e06e2f7a5944176
황혜인 <hhi9037@naver.com>
2024-04-18 16:22:40 +0900

    [style] 로그인페이지 <a>에서 <button>으로 수정

--- OMO-project/src/pages/login/Login.jsx
+++ OMO-project/src/pages/login/Login.jsx
@@ -9,19 +9,20 @@ const Login = () => (
       <h2 className={styles["login-title"]}>로그인</h2>
 
       <div className={styles["login-community-container"]}>
-        <a href="#" className={styles["login-community-kakao"]}>
+        <button className={styles["login-community-kakao"]}>
           <img src={kakao} className={styles["login-community-kakao-icon"]}></img>
           <span className={styles["login-community-kakao-letter"]}>카카오 로그인</span>
-        </a>
-        <a href="#" className={styles["login-community-naver"]}>
+        </button>
+
+        <button className={styles["login-community-naver"]}>
           <img src={naver} className={styles["login-community-naver-icon"]}></img>
           <span className={styles["login-community-naver-letter"]}>네이버 로그인</span>
-        </a>
+        </button>
 
-        <a href="#" className={styles["login-community-google"]}>
+        <button className={styles["login-community-google"]}>
           <img src={google} className={styles["login-community-google-icon"]}></img>
           <span className={styles["login-community-google-letter"]}>구글 로그인</span>
-        </a>
+        </button>
       </div>
     </section>
   </>
