7969d67bccc61d704ae2e5ba286fdfdac88f4824
황혜인 <hhi9037@naver.com>
2024-05-14 20:28:13 +0900

    [fix] 캘린더 컴포넌트 오류 수정

--- OMO-project/src/components/Calender/Calendar.jsx
+++ OMO-project/src/components/Calender/Calendar.jsx
@@ -1,90 +1,69 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css"; // css import 
+import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/esm/locale";
 import styles from "./Calendar.module.css";
 import { getMonth, getYear } from 'date-fns';
 import range from "lodash/range";
 
-
-
 const Calendar = () => {
-  
   const [startDate, setStartDate] = useState(new Date());
   const years = range(1800, getYear(new Date()) + 1, 1);
-  const months = [
-    "1월",
-    "2월",
-    "3월",
-    "4월",
-    "5월",
-    "6월",
-    "7월",
-    "8월",
-    "9월",
-    "10월",
-    "11월",
-    "12월",
-  ];
+  const months = range(0, 12); // 월을 숫자로 처리하도록 변경
+
   return (
     <DatePicker 
-    selected={startDate} 
-    onChange={date => setStartDate(date)}
-    locale={ko}                   // 한글로 변경
-    dateFormat="yyyy / MM / dd" // 시간 포맷 변경
-    showPopperArrow={false}       // 화살표 변경
-    className={styles["date-picker"]}
-    renderCustomHeader={({
-      date,
-      changeYear,
-      changeMonth,
-      decreaseMonth,
-      increaseMonth,
-      prevMonthButtonDisabled,
-      nextMonthButtonDisabled,
-    }) => (
-      <div className={styles["custom-header-container"]}>
-        <button className={styles["custom-header-button-left"]} onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
-          {"<"}
-        </button>
+      selected={startDate} 
+      onChange={date => setStartDate(date)}
+      locale={ko}
+      dateFormat="yyyy-MM-dd" // 일반적인 날짜 포맷으로 변경
+      showPopperArrow={false}
+      className={styles["date-picker"]}
+      renderCustomHeader={({
+        date,
+        changeYear,
+        changeMonth,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div className={styles["custom-header-container"]}>
+          <button className={styles["custom-header-button-left"]} onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+            {"<"}
+          </button>
 
-       
-        <select
-          className={styles["select-year"]}
-          value={getYear(date)}
-          onChange={({ target: { value } }) => changeYear(value)}
-        >
-          {years.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
+          <select
+            className={styles["select-year"]}
+            value={getYear(date)}
+            onChange={({ target: { value } }) => changeYear(value)}
+          >
+            {years.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
 
-        <select
-          className={styles["select-date"]}
-          value={months[getMonth(date)]}
-          onChange={({ target: { value } }) =>
-            changeMonth(months.indexOf(value))
-          }
-        >
-          {months.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-    
+          <select
+            className={styles["select-date"]}
+            value={getMonth(date)} // 현재 월의 숫자를 사용
+            onChange={({ target: { value } }) => changeMonth(Number(value))}
+          >
+            {months.map((option) => (
+              <option key={option} value={option}>
+                {option + 1}월 
+              </option>
+            ))}
+          </select>
 
-        <button  className={styles["custom-header-button-right"]} onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
-          {">"}
-        </button>
-      </div>
-    )}
-    
+          <button className={styles["custom-header-button-right"]} onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+            {">"}
+          </button>
+        </div>
+      )}
     />
   );
 };
 
-export default Calendar
-
+export default Calendar;
