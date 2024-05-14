e410e22f86fc07da49d28a1e47534648d4e53ca5
황혜인 <hhi9037@naver.com>
2024-05-14 21:56:47 +0900

    [setting] eslint prop-types 수정

--- OMO-project/.eslintrc.cjs
+++ OMO-project/.eslintrc.cjs
@@ -26,6 +26,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'no-unused-vars': 'warn', 
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
   },
 };
