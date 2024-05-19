63dce5c5ba9d31069e349b700482ace477b2aa3e
황혜인 <hhi9037@naver.com>
2024-05-19 16:31:44 +0900

    [feat] 고민게시판 게시글 불러오기 및 글쓰기 통신 완료

--- OMO-project/src/pages/Community/WorryBoard/WorryBoard.jsx
+++ OMO-project/src/pages/Community/WorryBoard/WorryBoard.jsx
@@ -9,7 +9,7 @@ import WritingButtonImg from "../../../assets/writing-button.png";
 import WriteWorryBoard from "../../../components/WritePost/WriteWorryBoard/WriteWorryBoard";
 
 const WorryBoard = () => {
-  const [data, setData] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [boardId, setBoardId] = useState(null);
   const [openModal, setOpenModal] = useState(false);
 
@@ -17,7 +17,7 @@ const WorryBoard = () => {
   const fetchData = async () => {
     try {
       const response = await axios.get('https://api.oneulmohae.co.kr/board/Trouble?page=1&size=10&sorting=createdAt');
-      setData(response.data);
+      setPosts(response.data.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -27,8 +27,7 @@ const WorryBoard = () => {
     fetchData();
   }, []);
 
-  console.log("get으로 불러온 데이터: ", data); // {data배열, pageInfo}
-
+  console.log("get으로 불러온 데이터: ", posts); // {data배열, pageInfo}
 
   // 게시글 작성
   const onCreate = async (title, content) => {
@@ -54,16 +53,11 @@ const WorryBoard = () => {
       const newPost = response.data;
       console.log('post데이터:', newPost); // {data+사용자정보까지}
       setBoardId(newPost.boardId); // 새로 생성된 게시글의 ID를 boardId로 설정
-      setData(prevData => [
-        {
-          createdDate: newPost.createdDate,
-          title: newPost.title,
-          content: newPost.content,
-          type: "TROUBLE",
-        },
-        ...prevData
+      setPosts(prevPosts => [
+        newPost,
+        ...prevPosts
       ]);
-      
+
     } catch (error) {
       console.error("Error creating post:", error);
       if (error.response) {
@@ -74,7 +68,6 @@ const WorryBoard = () => {
       alert("게시글 작성 중 오류가 발생했습니다.");
     }
   };
-
 
   return (
     <>
@@ -87,10 +80,10 @@ const WorryBoard = () => {
       </div>
 
       {/* 게시글 리스트 */}
-      {data.length === 0 ? (
+      {posts.length === 0 ? (
         <div>글 작성 내역이 없습니다. 우측 하단에 있는 글쓰기 버튼을 통해 게시글을 작성해주세요.</div>
       ) : (
-        <CommunityWorryPostList communityWorryPostList={data.data} />
+        <CommunityWorryPostList communityWorryPostList={posts} />
       )}
 
       {/* 스크롤 */}
