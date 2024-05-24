18750a2e860bc29af52fcdca98b9099707669652
황혜인 <hhi9037@naver.com>
2024-05-24 21:34:23 +0900

    [feat] 마이페이지 관심 목록 통신 완료

--- OMO-project/src/components/MypageListBox/MypageListBox.jsx
+++ OMO-project/src/components/MypageListBox/MypageListBox.jsx
@@ -1,6 +1,8 @@
 import styles from "./MypageListBox.module.css";
 import Like from "../../assets/detail/purple-thumb.png";
+import EmptyLike from "../../assets/detail/empty-thumb.png";
 import Jjim from "../../assets/detail/red-heart.png";
+import EmptyJjim from "../../assets/detail/empty-heart.png";
 import {useNavigate} from "react-router-dom";
 
 export const MypageListBox = (props) => {
@@ -10,22 +12,24 @@ export const MypageListBox = (props) => {
       <div
         className={styles["mypage-list-box-inner-container"]}
         onClick={() => {
-          navigate(`/DetailMenu/${props.id}`);
+          navigate(`/DetailMenu/${props.id}/${props.place_name}`);
         }}
       >
-        <span className={styles["mypage-list-box-title"]}>{props.title}</span>
+        <span className={styles["mypage-list-box-title"]}>{props.place_name}</span>
 
         <div className={styles["mypage-list-box-like-jjim-container"]}>
           <div className={styles["mypage-list-box-jjim"]}>
-            <img src={Jjim} alt="찜 아이콘" /> <span className={styles["mypage-list-box-jjim-number"]}> {props.jjim}</span>
+            <img src={props.myMine ? Jjim : EmptyJjim} alt="찜 아이콘" /> 
+            <span className={styles["mypage-list-box-jjim-number"]}> {props.mine}</span>
           </div>
           <div className={styles["mypage-list-box-like"]}>
-            <img src={Like} alt="좋아요 아이콘" /> <span className={styles["mypage-list-box-like-number"]}> {props.like}</span>
+            <img src={props.myRecommend ? Like : EmptyLike} alt="좋아요 아이콘" /> 
+            <span className={styles["mypage-list-box-like-number"]}> {props.recommend}</span>
           </div>
         </div>
 
-        <span className={styles["mypage-list-box-address-brief"]}>{props.addressBrief}</span>
-        <span className={`${styles["mypage-list-box-runtime"]} ${props.runTime === "영업 준비 중" ? styles["ready"] : ""} `}> {props.runTime}</span>
+        <span className={styles["mypage-list-box-address-brief"]}>{props.address_name}</span>
+        <span className={styles["mypage-list-box-phone"]}> {props.phone}</span>
         <img className={styles["mypage-list-box-img1"]} src={props.src1} />
         <img className={styles["mypage-list-box-img2"]} src={props.src2} />
         <img className={styles["mypage-list-box-img3"]} src={props.src3} />
