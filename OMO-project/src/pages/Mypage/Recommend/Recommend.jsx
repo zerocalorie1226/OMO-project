5dab3d42c59bb29d85ba6048d3a25eb90925dac5
황혜인 <hhi9037@naver.com>
2024-05-25 01:23:59 +0900

    [feat] 추천한 장소 통신 완료

--- OMO-project/src/pages/Mypage/Recommend/Recommend.jsx
+++ OMO-project/src/pages/Mypage/Recommend/Recommend.jsx
@@ -1,35 +1,59 @@
+import axios from "axios";
 import styles from "../Recommend/Recommend.module.css";
 import Mypage from "../../../components/Mypage/Mypage";
 import {MypageListBox} from "../../../components/MypageListBox/MypageListBox";
 import {ScrollToTop} from "../../../components/ScrollToTop/ScrollToTop";
 import RecommendIcon from "../../../assets/my-page/my-info/empty-thumb.png";
+import {useEffect, useState} from "react";
 
-const Recommend = ({likeData}) => (
-  <>
-    <div className={styles["mypage-list-component-container"]}>
-      <h2 className={styles["mypage-list-title-container"]}>
-        <div>
-          <img src={RecommendIcon} alt="추천한 장소 아이콘" /> <span>추천한 장소</span>
-        </div>
-      </h2>
+const Recommend = () => {
+  const [recommendPosts, setRecommendPosts] = useState(null); // 초기값을 null로 설정
 
-      <section className={styles["mypage-list-container"]}>
-        <Mypage />
-        <div className={styles["mypage-list-box-container"]}>
-          {likeData.length === 0 ? (
-            <div className={styles["no-like-list"]}>추천한 장소가 없습니다. 장소 상세 페이지에서 추천 아이콘을 눌러보세요!</div>
-          ) : (
-            <div>
-              {likeData.map((el) => {
-                return <MypageListBox key={el.id} {...el} />;
-              })}
-            </div>
-          )}
-        </div>
-      </section>
-    </div>
-    <ScrollToTop />
-  </>
-);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://api.oneulmohae.co.kr/myPage/recommend?page=1&size=10", {
+        headers: {
+          Authorization: localStorage.getItem("accessToken"),
+        },
+      });
+      setRecommendPosts(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setRecommendPosts([]); // 오류 발생 시 빈 배열로 초기화
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className={styles["mypage-list-component-container"]}>
+        <h2 className={styles["mypage-list-title-container"]}>
+          <div>
+            <img src={RecommendIcon} alt="추천한 장소 아이콘" /> <span>추천한 장소</span>
+          </div>
+        </h2>
+
+        <section className={styles["mypage-list-container"]}>
+          <Mypage />
+          <div className={styles["mypage-list-box-container"]}>
+            {recommendPosts === null || recommendPosts.length === 0 ? (
+              <div className={styles["no-jjim-list"]}>관심 목록이 없습니다. 장소 상세 페이지에서 하트를 눌러보세요!</div>
+            ) : (
+              <div>
+                {recommendPosts.map((el) => {
+                  return <MypageListBox key={el.id} {...el} />;
+                })}
+              </div>
+            )}
+          </div>
+        </section>
+      </div>
+      <ScrollToTop />
+    </>
+  );
+};
 
 export default Recommend;
