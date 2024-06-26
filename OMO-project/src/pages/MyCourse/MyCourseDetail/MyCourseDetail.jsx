9178b78b3db00c870323446eee5f7b2bbe27a04a
황혜인 <hhi9037@naver.com>
2024-06-27 01:33:23 +0900

    [feat] 나만의 코스 상세페이지 GET 통신 완료

--- OMO-project/src/pages/MyCourse/MyCourseDetail/MyCourseDetail.jsx
+++ OMO-project/src/pages/MyCourse/MyCourseDetail/MyCourseDetail.jsx
@@ -1,76 +1,73 @@
-// 상세페이지
-
 import styles from "./MyCourseDetail.module.css";
 import {ScrollToTop} from "../../../components/ScrollToTop/ScrollToTop";
 import Share from "../../../components/MyCourse/Button/Share/Share";
 import {useNavigate, useParams} from "react-router-dom";
-import React, {useContext, useEffect, useState} from "react";
-import {MyCourseStateContext} from "../../../assets/context/MyCourseContext"
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import MyCourseDetailBox from "../../../components/MyCourseDetailBox/MyCourseDetailBox";
 import downArrow from "../../../assets/my-course/write/down-arrow.png";
 
 const MyCourseDetail = () => {
   const {id} = useParams();
-  const myCourseList = useContext(MyCourseStateContext);
-  const navigate = useNavigate();
-  const [data, setData] = useState();
+  const [detailData, setDetailData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const getStringDate = (date) => {
     const options = {year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric", hour12: false};
-    return new Intl.DateTimeFormat("ko-KR", options).format(date);
+    return new Intl.DateTimeFormat("ko-KR", options).format(new Date(date));
   };
 
   useEffect(() => {
-    if (myCourseList.length >= 1) {
-      const targetMyCourse = myCourseList.find((it) => parseInt(it.id) === parseInt(id));
-
-      if (targetMyCourse) {
-        setData(targetMyCourse);
-      } else {
-        // alert("존재하지 않는 코스입니다.")
-        navigate("/MyCourseMain", {replace: true});
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://api.oneulmohae.co.kr/mycourse/${id}`, {
+          headers: {
+            Authorization: localStorage.getItem("accessToken"),
+          },
+        });
+        setDetailData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError("데이터를 불러오는 데 실패했습니다.");
+        setLoading(false);
       }
-    }
-  }, [id, myCourseList]);
-  if (!data) {
-    return <div className={styles["loading"]}>로딩중입니다...</div>;
-  } else {
-    console.log("디테일 data.content: ", data.content);
-
-    const formatDate = (dateString) => {
-      const options = {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-      };
-      return new Date(dateString).toLocaleString("ko-KR", options);
     };
 
-    return (
-      <div className={styles["mycourse-detail-total-container"]}>
-        <div className={styles["mycourse-detail-title-container"]}>
-          <span>{data.courseName}</span>
-        </div>
-        <div className={styles["mycourse-detail-course-container"]}>
-          <div className={styles["mycourse-detail-course-item-container"]}>
-            {/* content와 time를 함께 순회하여 각 쌍의 요소를 렌더링 */}
-            {data.content.map((contentItem, index) => (
-              <React.Fragment key={index}>
-                <div className={styles["mycourse-detail-calendar-container"]}>{formatDate(data.time[index])}</div>
-                <MyCourseDetailBox {...contentItem} />
-                {index !== data.content.length - 1 && <img src={downArrow} className={styles["mycourse-data-box-down-arrow-img"]} />}
-              </React.Fragment>
-            ))}
-          </div>
-        </div>
-        <div className={styles["mycourse-detail-edit-share-button-container"]}>
-          <Share />
-        </div>
-        <ScrollToTop />
-      </div>
-    );
+    fetchData();
+  }, [id]);
+
+  if (loading) {
+    return <div className={styles["loading"]}>로딩 중입니다...</div>;
   }
+
+  if (error) {
+    return <div className={styles["error"]}>{error}</div>;
+  }
+
+  return (
+    <div className={styles["mycourse-detail-total-container"]}>
+      <div className={styles["mycourse-detail-title-container"]}>
+        <span>{detailData.courseName}</span>
+      </div>
+      <div className={styles["mycourse-detail-course-container"]}>
+        <div className={styles["mycourse-detail-course-item-container"]}>
+          {/* content와 time를 함께 순회하여 각 쌍의 요소를 렌더링 */}
+          {detailData.contents.map((contentItem, index) => (
+            <React.Fragment key={index}>
+              <div className={styles["mycourse-detail-calendar-container"]}>{getStringDate(contentItem.time)}</div>
+              <MyCourseDetailBox {...contentItem} />
+              {index !== detailData.contents.length - 1 && <img src={downArrow} className={styles["mycourse-data-box-down-arrow-img"]} />}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+      <div className={styles["mycourse-detail-edit-share-button-container"]}>
+        <Share />
+      </div>
+      <ScrollToTop />
+    </div>
+  );
 };
 
 export default MyCourseDetail;
