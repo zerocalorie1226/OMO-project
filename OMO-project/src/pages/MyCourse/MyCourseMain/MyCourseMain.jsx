0efd2e32240435438b487140d3a307fe252b6e93
황혜인 <hhi9037@naver.com>
2024-06-29 15:38:56 +0900

    [feat] 나만의 코스에 검색 기능 추가

--- OMO-project/src/pages/MyCourse/MyCourseMain/MyCourseMain.jsx
+++ OMO-project/src/pages/MyCourse/MyCourseMain/MyCourseMain.jsx
@@ -6,8 +6,10 @@ import {WritingButton} from "../../../components/WritingButton/WritingButton";
 import MyCourseList from "../../../components/MyCourse/MyCourseList/MyCourseList";
 import styles from "./MyCourseMain.module.css";
 import {Loading} from "../../../components/Loading/Loading";
+import ListSearch from "../../../components/ListSearch/ListSearch";
 
 const MyCourseMain = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [myCourseList, setMyCourseList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -32,6 +34,12 @@ const MyCourseMain = () => {
     fetchData();
   }, []);
 
+  const filteredData = myCourseList.filter((item) => item.courseName.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  const onSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   if (loading) {
     return <Loading />;
   }
@@ -43,11 +51,14 @@ const MyCourseMain = () => {
   return (
     <div className={styles["mycourse-container"]}>
       <h2 className={styles["mycourse-title"]}>나만의 코스</h2>
-      {myCourseList.length === 0 ? (
+     <div className={styles["mycourse-search-container"]}>
+      <ListSearch searchTerm={searchTerm} onSearch={onSearch} />
+      </div>
+      {filteredData &&filteredData.length === 0 ? (
         <div className={styles["no-boardlist"]}>글 작성 내역이 없습니다. 우측 하단에 있는 글쓰기 버튼을 통해 게시글을 작성해주세요.</div>
       ) : (
         <div className={styles["mycourse-list-container"]}>
-          <MyCourseList myCourseList={myCourseList} />
+          <MyCourseList myCourseList={filteredData} />
         </div>
       )}
 
