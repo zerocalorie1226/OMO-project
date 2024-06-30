03ee50fed21d08b9d015ac61a73bc52388c65d82
황혜인 <hhi9037@naver.com>
2024-06-30 17:35:30 +0900

    [feat] 나만의 코스 좋아요 기능 추가

--- OMO-project/src/pages/MyCourse/MyCourseDetail/MyCourseDetail.jsx
+++ OMO-project/src/pages/MyCourse/MyCourseDetail/MyCourseDetail.jsx
@@ -6,8 +6,9 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import MyCourseDetailBox from "../../../components/MyCourseDetailBox/MyCourseDetailBox";
 import downArrow from "../../../assets/my-course/write/down-arrow.png";
-import { Loading } from "../../../components/Loading/Loading";
+import {Loading} from "../../../components/Loading/Loading";
 import Like from "../../../assets/community/my-course-board/empty-thumb.png";
+import LikeClicked from "../../../assets/detail/purple-thumb.png";
 
 const MyCourseDetail = () => {
   const {id} = useParams();
@@ -20,6 +21,10 @@ const MyCourseDetail = () => {
     return new Intl.DateTimeFormat("ko-KR", options).format(new Date(date));
   };
 
+  // 좋아요 버튼 (이미지, 클릭토글)
+  const [imageSrcLike, setImageSrcLike] = useState(Like);
+  const [isClickedLike, setIsClickedLike] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -29,6 +34,7 @@ const MyCourseDetail = () => {
           },
         });
         setDetailData(response.data);
+        console.log("코스상세데이터: ", response.data);
         setLoading(false);
       } catch (error) {
         setError("데이터를 불러오는 데 실패했습니다.");
@@ -38,6 +44,61 @@ const MyCourseDetail = () => {
 
     fetchData();
   }, [id]);
+
+  // 좋아요 버튼
+  const handleClickLike = async () => {
+    try {
+      const response = await axios.put(
+        `https://api.oneulmohae.co.kr/mycourse/like/${detailData.courseId}`,
+        {}, 
+        {
+          headers: {
+            Authorization: localStorage.getItem("accessToken"),
+          },
+          withCredentials: true,
+        }
+      );
+
+      if (response.status === 200) {
+        console.log(response);
+        console.log("isClickedLike: ", isClickedLike);
+        const getResponse = await axios.get(`https://api.oneulmohae.co.kr/mycourse/${id}`, {
+          headers: {
+            Authorization: localStorage.getItem("accessToken"),
+          },
+        });
+        if (getResponse.status === 200) {
+          console.log(getResponse);
+          // 이미지 변경 및 카운트 업데이트
+          if (isClickedLike) {
+            setImageSrcLike(Like);
+            setIsClickedLike(false);
+          } else {
+            setImageSrcLike(LikeClicked);
+            setIsClickedLike(true);
+          }
+          setDetailData(getResponse.data);
+        } else {
+          console.error("게시글을 가져오는데 실패하였습니다.");
+        }
+      } else {
+        console.error("게시글을 가져오는데 실패하였습니다.");
+      }
+    } catch (error) {
+      console.error("게시글을 가져오는데 실패하였습니다.:", error);
+    }
+  };
+
+  // like 값을 사용하여 초기 상태 설정 (따봉 누른 것들 새로고침해도 유지하게끔)
+  // useEffect(() => {
+  //   if (detailData.myLiked) {
+  //     setImageSrcLike(LikeClicked);
+  //     setIsClickedLike(true);
+  //   } else {
+  //     setImageSrcLike(Like);
+  //     setIsClickedLike(false);
+  //   }
+  // }, [detailData.myLiked]);
 
   if (loading) {
     return <Loading />;
@@ -50,10 +111,18 @@ const MyCourseDetail = () => {
   return (
     <div className={styles["mycourse-detail-total-container"]}>
       <div className={styles["mycourse-detail-title-container"]}>
-        <span>{detailData.writerName}의 {detailData.courseName}</span>
+        <span>
+          {detailData.writerName}의 {detailData.courseName}
+        </span>
       </div>
-      <div className={styles["mycourse-detail-created-date-container"]}>
-        {/* <img src={Like} alt="좋아요 아이콘" /> <span className={styles["community-mycourse-list-like-number"]}> {detailData.likeCount}</span> */}
+      <div className={styles["mycourse-detail-like-container"]}>
+        <button className={styles["mycourse-detail-like-button"]} onClick={handleClickLike} type="button">
+          {" "}
+          <img src={imageSrcLike} alt="좋아요 아이콘" />{" "}
+        </button>{" "}
+        <span className={styles["community-mycourse-detail-like-number"]}> {detailData.likeCount}</span>
+      </div>
+      <div className={styles["mycourse-detail-created-date"]}>
         <span>작성 일자: {getStringDate(detailData.createdAt)}</span>
       </div>
       <div className={styles["mycourse-detail-course-container"]}>
@@ -68,9 +137,7 @@ const MyCourseDetail = () => {
           ))}
         </div>
       </div>
-      <div className={styles["mycourse-detail-edit-share-button-container"]}>
-        {/* <Share /> */}
-      </div>
+      <div className={styles["mycourse-detail-edit-share-button-container"]}>{/* <Share /> */}</div>
       <ScrollToTop />
     </div>
   );
