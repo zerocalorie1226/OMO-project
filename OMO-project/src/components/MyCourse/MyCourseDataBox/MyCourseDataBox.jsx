4d85b71808a3cb4d59de3f25bff05d8fb68aca13
조성찬 <kjyy07@naver.com>
2024-07-07 16:51:27 +0900

    [feat] 나만의 코스 글쓰기에서 찾기 3가지랑, 들어갔을때(MyCourseDataBox) 가게들 이미지 보이게 하기

--- OMO-project/src/components/MyCourse/MyCourseDataBox/MyCourseDataBox.jsx
+++ OMO-project/src/components/MyCourse/MyCourseDataBox/MyCourseDataBox.jsx
@@ -1,6 +1,7 @@
 import styles from "./MyCourseDataBox.module.css";
 import Like from "../../../assets/detail/purple-thumb.png";
 import Jjim from "../../../assets/detail/red-heart.png";
+import defaultDetailIcon from "../../../assets/detail/defaultDetailIcon.png";
 
 export const MyCourseDataBox = ({data}) => {
   return (
@@ -11,20 +12,29 @@ export const MyCourseDataBox = ({data}) => {
 
           <div className={styles["mycourse-data-box-box-like-jjim-container"]}>
             <div className={styles["mycourse-data-box-box-jjim"]}>
-              <img src={Jjim} alt="찜 아이콘" style={{position: "absolute", top: "2px", width: "20px", height: "20px"}} />{" "}
+              <img src={Jjim} alt="찜 아이콘" style={{position: "absolute", top: "2px", width: "20px", height: "20px"}} />
               <span className={styles["mycourse-data-box-box-jjim-number"]}> {data.mine}</span>
             </div>
             <div className={styles["mycourse-data-box-box-like"]}>
-              <img src={Like} alt="좋아요 아이콘" style={{position: "absolute", top: "0px", width: "20px", height: "20px"}} />{" "}
+              <img src={Like} alt="좋아요 아이콘" style={{position: "absolute", top: "0px", width: "20px", height: "20px"}} />
               <span className={styles["mycourse-data-box-box-like-number"]}> {data.recommend}</span>
             </div>
           </div>
 
           <span className={`${styles["mycourse-data-box-box-phone"]} ${!data.phone ? styles["not_provided"] : ""}`}>{data.phone ? data.phone : "전화번호 미제공"}</span>
           <span className={styles["mycourse-data-box-box-address-brief"]}>{data.road_address_name}</span>
-          <img className={styles["mycourse-data-box-box-img1"]} src={data.src1} />
-          <img className={styles["mycourse-data-box-box-img2"]} src={data.src2} />
-          <img className={styles["mycourse-data-box-box-img3"]} src={data.src3} />
+
+          {data.src1 ? (
+            <img className={styles["mycourse-data-box-box-img1"]} src={data.src1} alt="list image 1" />
+          ) : (
+            <img className={styles["mycourse-data-box-box-img1"]} src={defaultDetailIcon} alt="default list image 1" />
+          )}
+          
+          {data.src2 ? (
+            <img className={styles["mycourse-data-box-box-img2"]} src={data.src2} alt="list image 2" />
+          ) : (
+            <img className={styles["mycourse-data-box-box-img2"]} src={defaultDetailIcon} alt="default list image 2" />
+          )}
         </div>
       </div>
       <img src={data.downArrow} className={styles["mycourse-data-box-down-arrow-img"]} />
