779c28d6c851d7e4d11cd0f7d225d86839bb3537
황혜인 <hhi9037@naver.com>
2024-07-09 19:26:57 +0900

    [feat] 나만의 코스 커뮤니티 공유 기능  추가

--- OMO-project/src/components/MyCourse/Button/Share/Share.jsx
+++ OMO-project/src/components/MyCourse/Button/Share/Share.jsx
@@ -1,22 +1,40 @@
 import styles from "./Share.module.css";
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
-const Share = () => {
+const Share = ({ courseId }) => {
   const navigate = useNavigate();
+
+  const handleShare = async () => {
+    try {
+      const response = await axios.patch(`https://api.oneulmohae.co.kr/mycourse/share/${courseId}`, {}, {
+        headers: {
+          Authorization: localStorage.getItem("accessToken"),
+        },
+      });
+
+      if (response.status === 200) {
+        alert("공유되었습니다.");
+        navigate("/MyCourseBoard", { replace: true });
+      } else {
+        console.error("공유에 실패하였습니다.");
+      }
+    } catch (error) {
+      console.error("공유에 실패하였습니다.", error);
+    }
+  };
 
   return (
     <div className={styles["edit-share-button-container"]}>
       <button
         type="button"
         className={styles["share-button"]}
-        onClick={() => {
-          alert("공유되었습니다.");
-          navigate("/MyCourseBoard", { replace: true });
-        }}
+        onClick={handleShare}
       >
         커뮤니티에 공유
       </button>
     </div>
   );
 };
+
 export default Share;
