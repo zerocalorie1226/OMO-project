1e9769150d9a2d954a049dd2bf8fd4d9192bb153
황혜인 <hhi9037@naver.com>
2024-07-30 19:21:56 +0900

    [feat] 회원탈퇴할때 위치 기록 로컬스토리지에서 삭제

--- OMO-project/src/components/MypageProfileSetting/MembershipWithdrawal/MembershipWithdrawal.jsx
+++ OMO-project/src/components/MypageProfileSetting/MembershipWithdrawal/MembershipWithdrawal.jsx
@@ -27,6 +27,7 @@ const MembershipWithdrawal = ({setIsLoggedIn}) => {
           localStorage.removeItem("memberId");
           localStorage.removeItem("recentData");
           localStorage.removeItem('isLoggedIn');
+          localStorage.removeItem('savedCoordinates');
           navigate("/", {replace: true});
         } else {
           alert("탈퇴에 실패하였습니다. 다시 시도해 주세요.");
