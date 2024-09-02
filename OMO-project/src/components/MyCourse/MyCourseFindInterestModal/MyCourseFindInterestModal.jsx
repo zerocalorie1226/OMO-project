16648f45ea6adcef64e149f1dd241ff03628c4fb
조성찬 <kjyy07@naver.com>
2024-09-02 15:12:51 +0900

    [feat] 데이터 받아오기

--- OMO-project/src/components/MyCourse/MyCourseFindInterestModal/MyCourseFindInterestModal.jsx
+++ OMO-project/src/components/MyCourse/MyCourseFindInterestModal/MyCourseFindInterestModal.jsx
@@ -1,15 +1,17 @@
 import styles from "./MyCourseFindInterestModal.module.css";
-import {MyCourseItemListBox} from "../MyCourseItemListBox/MyCourseItemListBox";
+import { MyCourseItemListBox } from "../MyCourseItemListBox/MyCourseItemListBox";
 import ModalClose from "./../../../assets/modal-close.png";
-import {useEffect, useState, useCallback} from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import axios from "axios";
-import {Loading} from "../../Loading/Loading";
+import { Loading } from "../../Loading/Loading";
 
-const MyCourseFindInterestModal = ({interestModal, setInterestModal, state, setState, setPlaceName, setPlaceId}) => {
+const MyCourseFindInterestModal = ({ interestModal, setInterestModal, state, setState, setPlaceName, setPlaceId }) => {
   const [interestPosts, setInterestPosts] = useState([]);
-  const [maxPage, setMaxPage] = useState(0);
   const [pagination, setPagination] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true); // 데이터가 더 있는지 여부
+
+  const observer = useRef();
 
   const fetchData = async (page) => {
     setLoading(true);
@@ -22,8 +24,11 @@ const MyCourseFindInterestModal = ({interestModal, setInterestModal, state, setS
 
       const posts = response.data ? response.data : [];
 
-      setInterestPosts((prevPosts) => [...prevPosts, ...posts]);
-      setMaxPage(response.data?.totalPages || 0);
+      if (posts.length === 0) {
+        setHasMore(false); // 받아온 데이터가 없으면 더 이상 가져올 데이터가 없다고 판단
+      } else {
+        setInterestPosts((prevPosts) => [...prevPosts, ...posts]);
+      }
     } catch (error) {
       console.error("데이터를 가져오는데 실패했습니다.", error);
     } finally {
@@ -31,27 +36,34 @@ const MyCourseFindInterestModal = ({interestModal, setInterestModal, state, setS
     }
   };
 
-  const handleScroll = useCallback(() => {
-    if (loading || pagination >= maxPage) return;
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-      setPagination((prevPage) => prevPage + 1);
-    }
-  }, [loading, pagination, maxPage]);
+  const lastElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPagination((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   useEffect(() => {
-    fetchData(pagination);
-  }, [pagination]);
+    if (hasMore) {
+      fetchData(pagination);
+    }
+  }, [pagination, hasMore]);
 
   useEffect(() => {
     // 모달이 열릴 때 배경 스크롤 막기
     document.body.style.overflow = "hidden";
-    window.addEventListener("scroll", handleScroll);
     return () => {
       // 모달이 닫힐 때 배경 스크롤 복원
       document.body.style.overflow = "auto";
-      window.removeEventListener("scroll", handleScroll);
     };
-  }, [handleScroll]);
+  }, []);
 
   const handleClickItem = (place_name, id) => {
     setPlaceName(place_name);
@@ -68,13 +80,34 @@ const MyCourseFindInterestModal = ({interestModal, setInterestModal, state, setS
             {!interestModal ? setInterestModal(true) : null}
           </button>
         </label>
-        {interestPosts.length === 0 ? (
+        {interestPosts.length === 0 && !loading ? (
           <div className={styles["no-jjim-list"]}>관심 목록이 없습니다. 장소 상세 페이지에서 하트를 눌러보세요!</div>
         ) : (
           <div className={styles["mycourse-find-interest-modal-list-box-container"]}>
-            {interestPosts.map((el) => (
-              <MyCourseItemListBox key={el.id} state={state} setState={setState} el={el} onClick={(place_name, id) => handleClickItem(place_name, id)} />
-            ))}
+            {interestPosts.map((el, index) => {
+              if (index === interestPosts.length - 1) {
+                return (
+                  <MyCourseItemListBox
+                    ref={lastElementRef}
+                    key={el.id}
+                    state={state}
+                    setState={setState}
+                    el={el}
+                    onClick={(place_name, id) => handleClickItem(place_name, id)}
+                  />
+                );
+              } else {
+                return (
+                  <MyCourseItemListBox
+                    key={el.id}
+                    state={state}
+                    setState={setState}
+                    el={el}
+                    onClick={(place_name, id) => handleClickItem(place_name, id)}
+                  />
+                );
+              }
+            })}
             {loading && (
               <div>
                 <Loading />
