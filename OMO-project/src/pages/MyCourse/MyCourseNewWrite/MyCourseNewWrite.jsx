8a7f36d93fd871b7b078425803a50b92000de22a
황혜인 <hhi9037@naver.com>
2024-09-08 17:45:00 +0900

    [feat] 나만의 코스 작성 USER or GUEST or 로그아웃 여부에 따른 분기 처리

--- OMO-project/src/pages/MyCourse/MyCourseNewWrite/MyCourseNewWrite.jsx
+++ OMO-project/src/pages/MyCourse/MyCourseNewWrite/MyCourseNewWrite.jsx
@@ -1,20 +1,28 @@
-//  작성페이지
-
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import MyCourseEditor from "../../../components/MyCourseEditor/MyCourseEditor";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 const MyCourseNewWrite = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const loggedIn = localStorage.getItem("isLoggedIn") === "true";
+    const memberRole = localStorage.getItem("memberRole");
+
     if (!loggedIn) {
       alert("로그인 후 이용 가능한 서비스입니다.");
       navigate("/Login", { replace: true });
+      return;
+    }
+
+    if (memberRole === "GUEST") {
+      alert("회원정보 입력이 필요합니다.");
+      navigate("/Signup", { replace: true });
+      return;
     }
   }, [navigate]);
 
   return <MyCourseEditor />;
 };
+
 export default MyCourseNewWrite;
